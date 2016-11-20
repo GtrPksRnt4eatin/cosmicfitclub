35f041b22bc884509f951317e63d50ceb0d46973
Maxwell Klein <bklein@vtsmedical.com>
2016-11-20 14:53:43 -0500

    imgscroller

--- js/index.js
+++ js/index.js
@@ -4,8 +4,24 @@ $(document).ready(function() {
     '/carousel/img01.jpg', 
     '/carousel/img02.jpg', 
     '/carousel/img03.jpg', 
-    '/carousel/img04.jpg'
+    '/carousel/img04.jpg',
+    '/carousel/img05.jpg',
+    '/carousel/img06.jpg',
   ];
+
+  $('#imgscroller').slick({
+  	centerMode: true,
+  	centerPadding: '60px',
+  	variableWidth: true,
+    slidesToShow: 3,
+  	infinite: true,
+  	autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 1
+  	
+  });
+
+  images.forEach( function(path) { var elem =  $('#imgscroller').slick('slickAdd',`<div><img style='max-height: 100%; margin: 0 10px;' src='${ path }' /></div>`); });
 
   //var imgscroller = new ImageScroller( id('imgscroller_container') );
   //imgscroller.load_images(images);
--- ruby/routes.rb
+++ ruby/routes.rb
@@ -6,14 +6,13 @@ get '*/:file.js'   do send_file "js/#{params[:file]}.js"   end
 get '*/:file.css'  do send_file "css/#{params[:file]}.css" end
 
 get /.*\.(jpeg|jpg|png|gif|ico|svg)/ do
-  puts request.path 
-  puts "img#{request.path}"
-  puts File.exist? "img#{request.path}"
   return 404 unless File.exist? "img#{request.path}"
   send_file "img#{request.path}"
 end
 
 get /.*\.(ttf|woff)/ do
-  return 404 unless File.exist? "fonts#{request.path}"
-  send_file "fonts#{request.path}"
+  path = request.path.gsub('fonts/', '');
+  puts "fonts#{path}"
+  return 404 unless File.exist? "fonts#{path}"
+  send_file "fonts#{path}"
 end
