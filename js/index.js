042881c3ba27ce22b481b3bad97cb0303e60e97a
Maxwell Klein <bklein@vtsmedical.com>
2016-12-06 20:04:19 -0500

    layout updates

--- js/index.js
+++ js/index.js
@@ -24,4 +24,49 @@ $(document).ready(function() {
 
   images.forEach( function(path) { var elem =  $('#imgscroller').slick('slickAdd',`<div><img class='sliderimg' src='${ path }' /></div>`); });
 
+  $('#logo').on('click', function(e) {
+    window.location.href = '/';
+  });
+
+  $('#menu li').on('click', function(e) {
+    window.location.href = e.target.getAttribute('href');
+  });
+
 });
+
+function tintImage(imgElement,tintColor) {
+    // create hidden canvas (using image dimensions)
+    var canvas = document.createElement("canvas");
+    canvas.width = imgElement.offsetWidth;
+    canvas.height = imgElement.offsetHeight;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(imgElement,0,0);
+
+    var map = ctx.getImageData(0,0,320,240);
+    var imdata = map.data;
+
+    // convert image to grayscale
+    var r,g,b,avg;
+    for(var p = 0, len = imdata.length; p < len; p+=4) {
+        r = imdata[p]
+        g = imdata[p+1];
+        b = imdata[p+2];
+        // alpha channel (p+3) is ignored           
+
+        avg = Math.floor((r+g+b)/3);
+
+        imdata[p] = imdata[p+1] = imdata[p+2] = avg;
+    }
+
+    ctx.putImageData(map,0,0);
+
+    // overlay filled rectangle using lighter composition
+    ctx.globalCompositeOperation = "lighter";
+    ctx.globalAlpha = 0.5;
+    ctx.fillStyle=tintColor;
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+
+    // replace image source with canvas data
+    imgElement.src = canvas.toDataURL();
+}
