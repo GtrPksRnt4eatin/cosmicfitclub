30f7c53364bfb8e67610ef9faf35e708dec166c7
Maxwell Klein <bklein@vtsmedical.com>
2016-12-31 21:35:08 -0500

    events styles

--- site/pages/events/events.js
+++ site/pages/events/events.js
@@ -5,6 +5,7 @@ var data = {
 $(document).ready(function() {
 
   rivets.bind(document.body, { data: data } );
+  rivets.formatters.dayofmo  = function(val) { return moment(val).format('Do') };
   rivets.formatters.dayofwk  = function(val) { return moment(val).format('ddd') };
   rivets.formatters.date     = function(val) { return moment(val).format('MMM Do') };
   rivets.formatters.time     = function(val) { return moment(val).format('h:mm a') };
