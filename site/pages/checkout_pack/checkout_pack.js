3360db8563b45831a30bdb3b0dc0cfb3e9ce3650
Maxwell Klein <bklein@vtsmedical.com>
2017-01-03 02:45:19 -0500

    no html escape

--- site/pages/checkout_pack/checkout_pack.js
+++ site/pages/checkout_pack/checkout_pack.js
@@ -1,36 +1,36 @@
-      $(document).ready( function() {
-        
-        var handler = StripeCheckout.configure({
-          zipCode: true,
-          locale: 'auto',
-          billingAddress: true,
-          key: STRIPE_PUBLIC_KEY,
+$(document).ready( function() {
+  
+  var handler = StripeCheckout.configure({
+    zipCode: true,
+    locale: 'auto',
+    billingAddress: true,
+    key: STRIPE_PUBLIC_KEY,
 
-          token: function(token) {
-            data = {
-              "type":  "package",
-              "plan_id": PACKAGE['id'],
-              "token": token
-            }
+    token: function(token) {
+      data = {
+        "type":  "package",
+        "plan_id": PACKAGE['id'],
+        "token": token
+      }
 
-            $.post('/stripe/charge', JSON.stringify( data ) )
-              .done(function() {
-                window.location.href = 'checkout/complete';
-              })
-              .fail(function() {
-                alert('Your Card Has not Been Charged. You already have a Membership, Sign in to Modify it.')
-              });
-          }
-
+      $.post('/stripe/charge', JSON.stringify( data ) )
+        .done(function() {
+          window.location.href = 'checkout/complete';
+        })
+        .fail(function() {
+          alert('Your Card Has not Been Charged. You already have a Membership, Sign in to Modify it.')
         });
+    }
 
-        id('checkout').addEventListener('click', function(e) {
-          handler.open({
-            name: 'Cosmic Fit Club',
-            description: PACKAGE['name'],
-            image: 'https://cosmicfit.herokuapp.com/background-blu.jpg',
-            amount: PACKAGE['pass_price'] * PACKAGE['num_passes']
-          })
-        });
+  });
 
-      });
+  id('checkout').addEventListener('click', function(e) {
+    handler.open({
+      name: 'Cosmic Fit Club',
+      description: PACKAGE['name'],
+      image: 'https://cosmicfit.herokuapp.com/background-blu.jpg',
+      amount: PACKAGE['pass_price'] * PACKAGE['num_passes']
+    })
+  });
+
+});
