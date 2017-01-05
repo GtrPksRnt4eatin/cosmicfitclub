5118ffd28ad44d25915cea0347988f069114b54d
Maxwell Klein <bklein@vtsmedical.com>
2017-01-05 02:36:54 -0500

    test

--- auth/omniauth.rb
+++ auth/omniauth.rb
@@ -14,7 +14,7 @@ module Sinatra
         #provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET'], {access_type: "offline", prompt: "consent", scope: 'userinfo.email, userinfo.profile'}
       end
 
-      app.get 'omni/:provider/callback' do
+      app.get '/auth/omni/:provider/callback' do
         auth = request.env['omniauth.auth']
 
         data = {
@@ -38,11 +38,15 @@ module Sinatra
 
         session[:user] = user
 
-        redirect '/userpage '
+        redirect '/userpage'
       end
 
       app.get 'omni/:provider/deauthorized' do
         "App Deauthorized"
+      end
+
+      app.get 'omni/failure' do
+        render_page :omnifailure
       end
 
     end
