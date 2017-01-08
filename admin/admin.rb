ba7bb31dadbd92363da366ac5aa5b10ce0ba5709
Maxwell Klein <bklein@vtsmedical.com>
2017-01-08 16:51:26 -0500

    spacing

--- admin/admin.rb
+++ admin/admin.rb
@@ -3,12 +3,11 @@ require 'sinatra/base'
 class CFCAdmin < Sinatra::Base
 
   enable :sessions	
+  set :root, File.dirname(__FILE__)
 
   helpers  Sinatra::ViewHelpers
   register Sinatra::PageFolders
   register Sinatra::SharedResources
-
-  set :root, File.dirname(__FILE__)
 
   get( '/'         ) { render_page :index    }
   get( '/carousel' ) { render_page :carousel }
