75067044420f5f8d7f605bda2d9c43bdac0e44fc
Maxwell Klein <bklein@vtsmedical.com>
2017-01-27 03:33:42 -0500

    user page

--- site/pages/waiver/waiver.js
+++ site/pages/waiver/waiver.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   pad = $("#signaturepad").jSignature()
 
   id('submit').addEventListener('click', function() {
-    $.post('/waiver', pad.jSignature("getData", "svg")[1] );
+    $.post('/waiver', pad.jSignature("getData", "svg")[1] ).done(function() { window.location.href = '/user'; });
   })
 });
