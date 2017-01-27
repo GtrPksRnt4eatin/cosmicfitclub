32455a6e76ed3f629b45efe2f91f63a95b168d52
Maxwell Klein <bklein@vtsmedical.com>
2017-01-27 00:13:46 -0500

    syntax

--- auth/pages/activate/activate.js
+++ auth/pages/activate/activate.js
@@ -1,14 +1,13 @@
 $(document).ready( function() {
 
 	$('#myform').submit(function(e){
-    e.preventDefault();
-    $.ajax({
+      e.preventDefault();
+      $.ajax({
         url:'/auth/password',
         type:'post',
         data:$('#myform').serialize(),
         success:function() { window.location.href = '/user';  },
         fail: function()   { $('#myform').shake(); }
-        }
     });
 
 });
