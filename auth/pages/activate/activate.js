8ed48b3a346659ee8c107b8bbc66d0752712c1b5
Maxwell Klein <bklein@vtsmedical.com>
2017-01-27 00:06:37 -0500

    password shake

--- auth/pages/activate/activate.js
+++ auth/pages/activate/activate.js
@@ -1,3 +1,14 @@
-function submit() {
-	
-}
+$(document).ready( function() {
+
+	$('#myform').submit(function(e){
+    e.preventDefault();
+    $.ajax({
+        url:'/auth/password',
+        type:'post',
+        data:$('#myform').serialize(),
+        success:function() { window.location.href = '/user';  },
+        fail: function()   { $('#myform').shake(); }
+        }
+    });
+
+});
