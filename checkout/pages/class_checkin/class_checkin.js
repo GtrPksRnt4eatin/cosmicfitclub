6790b78fda9b4514831da48e75f60ca286c43e20
Maxwell Klein <bklein@vtsmedical.com>
2017-05-21 01:06:31 -0400

    front door camera

--- checkout/pages/class_checkin/class_checkin.js
+++ checkout/pages/class_checkin/class_checkin.js
@@ -1,6 +1,23 @@
 $(document).ready( function() {
+
+  include_rivets_dates();
   
   var binding = rivets.bind( $('body'), { data: data } );
   var stripe  = Stripe(STRIPE_PUBLIC_KEY);
 
+  $('#customers').chosen();
+
+  $('#customers').on('change', on_customer_selected );
 });
+
+function on_customer_selected(e) {
+  $.get(`/models/customers/${parseInt(e.target.value)}/class_passes`, function(resp) {
+    data.customer.class_passes = resp;
+  }, 'json');
+
+  $.get(`/models/customers/${parseInt(e.target.value)}/status`, function(resp) {
+    console.log(resp);
+    data.customer.membership_status = resp;
+    console.log(data.customer.membership_status);
+  }, 'json');
+}
