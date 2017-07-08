5a5e84cd51ae874453fa275491f4dd7a6a58d7e6
Maxwell Klein <bklein@vtsmedical.com>
2017-07-08 14:22:46 -0400

    event image update bug

--- models/Events.rb
+++ models/Events.rb
@@ -142,7 +142,8 @@ class EventRoutes < Sinatra::Base
     if Event[params[:id]].nil?
       event = Event.create(name: params[:name], description: params[:description], :starttime => params[:starttime], image: params[:image] )
     else
-      event = Event[params[:id]].update_fields(params, [ :name, :description, :starttime ] )
+      event = Event[params[:id]]
+      event.update_fields(params, [ :name, :description, :starttime ] )
       event.update( :image => params[:image] ) unless params[:image].nil?
     end
     status 200
