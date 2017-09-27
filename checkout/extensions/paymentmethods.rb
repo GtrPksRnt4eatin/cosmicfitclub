20a18e1975990ec4feb8bc6239e5d37be8d39d6a
Maxwell Klein <bklein@vtsmedical.com>
2017-09-27 00:16:49 -0400

    handle charge errors

--- checkout/extensions/paymentmethods.rb
+++ checkout/extensions/paymentmethods.rb
@@ -10,6 +10,8 @@ module Sinatra
       description = "#{custy.name} purchased #{params[:description]}"  
       charge = StripeMethods::charge_card(params[:token], params[:amount], params[:email], description, params[:metadata])
       CustomerPayment.create(:customer => custy, :stripe_id => charge.id, :amount => params[:amount], :reason => params[:description], :type => 'new card').to_json
+    rescue Exception => ex
+      halt 400 ex.message
     end
 
     def charge_saved_card
