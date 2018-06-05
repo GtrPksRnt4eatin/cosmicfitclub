d15014142dbcd8a78bd480612cd12e3bf85736cb
Maxwell Klein <bklein261@gmail.com>
2018-06-05 19:24:52 -0400

    https cam

--- admin/pages/cameras/cameras.js
+++ admin/pages/cameras/cameras.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 function fetch_image() {
   $.ajax({
     type: "GET",
-    url: "http://cosmicfitclub.ddns.net:5051/cgi-bin/jpeg",
+    url: "https://cosmicfitclub.ddns.net:5051/cgi-bin/camera",
     async: false,
     headers: {
       "Authorization": "Basic " + btoa("admin:12345")
