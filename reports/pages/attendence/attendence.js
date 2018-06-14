4dac05ec187f7f5802ada0397623b31d1409b3d9
Maxwell Klein <bklein261@gmail.com>
2018-06-14 01:11:12 -0400

    slim update

--- reports/pages/attendence/attendence.js
+++ reports/pages/attendence/attendence.js
@@ -11,4 +11,10 @@ $(document).ready( function() {
   matches = /(\d{4}-\d\d-\d\d) to (\d{4}-\d\d-\d\d)/.exec(data.daterange);
   params = { from: matches[1], to: matches[2] };
   $.get('attendence_list.json', params, ctrl.on_list, 'json')
+  initialize_rivets();
 })
+
+function initialize_rivets() {
+  include_rivets_dates();
+  rivets.bind(document.body, { data: data, ctrl: ctrl } );
+}
