869615756f26486574390daecd75377b5c739cee
Maxwell Klein <bklein261@gmail.com>
2018-06-14 19:55:45 -0400

    first daterange

--- reports/pages/attendence/attendence.js
+++ reports/pages/attendence/attendence.js
@@ -8,13 +8,17 @@ ctrl = {
 }
 
 $(document).ready( function() {
-  matches = /(\d{4}-\d\d-\d\d) to (\d{4}-\d\d-\d\d)/.exec(data.daterange);
-  params = { from: matches[1], to: matches[2] };
-  $.get('attendence_list.json', params, ctrl.on_list, 'json')
   initialize_rivets();
+  get_data();
 })
 
 function initialize_rivets() {
   include_rivets_dates();
   rivets.bind(document.body, { data: data, ctrl: ctrl } );
+}
+
+function get_data() {
+  matches = /(\d{4}-\d\d-\d\d) to (\d{4}-\d\d-\d\d)/.exec(data.daterange);
+  params = matches ? { from: matches[1], to: matches[2] } : {};
+  $.get('attendence_list.json', params, ctrl.on_list, 'json')
 }
