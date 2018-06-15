ad9dfc372629411b40d60aceddd0927aeb85eee3
Maxwell Klein <bklein261@gmail.com>
2018-06-15 00:20:07 -0400

    daterange handler

--- reports/pages/attendence/attendence.js
+++ reports/pages/attendence/attendence.js
@@ -4,6 +4,11 @@ data = {
 }
 
 ctrl = {
+  get_list: function() {
+    matches = /(\d{4}-\d\d-\d\d) to (\d{4}-\d\d-\d\d)/.exec(data.daterange);
+    params = matches ? { from: matches[1], to: matches[2] } : {};
+    $.get('attendence_list.json', params, ctrl.on_list, 'json')
+  },
   on_list: function(list) { 
   	data.list = list; 
   }
@@ -11,16 +16,10 @@ ctrl = {
 
 $(document).ready( function() {
   initialize_rivets();
-  get_data();
+  ctrl.get_list();
 })
 
 function initialize_rivets() {
   include_rivets_dates();
   rivets.bind(document.body, { data: data, ctrl: ctrl } );
-}
-
-function get_data() {
-  matches = /(\d{4}-\d\d-\d\d) to (\d{4}-\d\d-\d\d)/.exec(data.daterange);
-  params = matches ? { from: matches[1], to: matches[2] } : {};
-  $.get('attendence_list.json', params, ctrl.on_list, 'json')
 }
