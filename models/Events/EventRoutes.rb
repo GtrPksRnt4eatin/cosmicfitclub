c8306b196af445e681d1d5d809c58b78a35dfe31
Maxwell Klein <bklein261@gmail.com>
2018-08-04 16:09:37 -0400

    print debug

--- models/Events/EventRoutes.rb
+++ models/Events/EventRoutes.rb
@@ -215,8 +215,8 @@ class EventRoutes < Sinatra::Base
     tic = EventTicket[params[:tic_id]] or halt 404
     recipient = Customer[params[:recipient_id]] or halt 404
     p recipient.id
-    p  params[:session_ids]
-    tic.split( recipient.id, params[:session_ids] )
+    p params[:session_ids]
+    p tic.split( recipient.id, params[:session_ids] )
     status 204
   end
 
