ef57cd317125f71e9aee0d565368727c144c5e33
Maxwell Klein <bklein261@gmail.com>
2018-08-03 23:42:17 -0400

    wrong session

--- admin/pages/edit_ticket/edit_ticket.js
+++ admin/pages/edit_ticket/edit_ticket.js
@@ -1,10 +1,14 @@
 ctrl = {
   assign_recipient: function(e,m) {
-    $.post('/models/events/tickets/' + data.ticket.id + '/assign_recipient', { recipient_id: data.})
+    $.post('/models/events/tickets/' + data.ticket.id + '/assign_recipient', { recipient_id: data.recipient })
+     .done(function() { location.reload() } )
+     .fail( alert('Assigning Recipient Failed') )
   },
 
   split_ticket: function(e,m) {
-    
+    $.post('/models/events/tickets/' + data.ticket.id + '/assign_recipient', { recipient_id: data.recipient })
+     .done(function() { location.reload() } )
+     .fail( alert('Splitting Ticket Failed') )
   }
 }
 
