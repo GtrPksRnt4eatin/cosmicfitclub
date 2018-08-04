044c21a3aa85e7acccf14a62e04a46354623ce1a
Maxwell Klein <bklein261@gmail.com>
2018-08-03 23:36:26 -0400

    js parenthesis

--- admin/pages/edit_ticket/edit_ticket.js
+++ admin/pages/edit_ticket/edit_ticket.js
@@ -1,6 +1,6 @@
 ctrl = {
   assign_recipient: function(e,m) {
-    
+    $.post('/models/events/tickets/' + data.ticket.id + '/assign_recipient', { recipient_id: data.})
   },
 
   split_ticket: function(e,m) {
@@ -10,7 +10,7 @@ ctrl = {
 
 $(document).ready(function() { 
   initialize_rivets() 
-}
+})
 
 function initialize_rivets() {
   rivets.bind( document.body, { data: data, ctrl: ctrl } );
