644ac9dc2131609a700973007a456131a14da333
Maxwell Klein <bklein261@gmail.com>
2018-08-07 00:32:25 -0400

    list

--- frontdesk/pages/event_checkin/event_checkin.js
+++ frontdesk/pages/event_checkin/event_checkin.js
@@ -4,4 +4,5 @@ ctrl = {
 
 $(document).ready(function(){
   rivets.bind( document.body, { data: data, ctrl: ctrl } ); 
+  var eventlist = new List('eventlist', { valueNames: [ 'time', 'name'] } );
 });
