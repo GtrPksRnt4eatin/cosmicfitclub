b6e400216e40fc88f44b7966a72493e6cb25c9cf
Maxwell Klein <bklein261@gmail.com>
2018-08-07 00:50:06 -0400

    userview

--- frontdesk/pages/event_checkin/event_checkin.js
+++ frontdesk/pages/event_checkin/event_checkin.js
@@ -3,6 +3,10 @@ ctrl = {
 }
 
 $(document).ready(function(){
+  
+  userview = new UserView(id('userview_container'));
+
   rivets.bind( document.body, { data: data, ctrl: ctrl } ); 
-  var eventlist = new List('eventlist', { valueNames: [ 'time', 'name'] } );
+  
+  //var eventlist = new List('eventlist', { valueNames: [ 'time', 'name'] } );
 });
