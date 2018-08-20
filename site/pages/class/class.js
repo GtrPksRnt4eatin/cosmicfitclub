be13a0b015fd95e51ccc678114a3b51cc6c27312
Maxwell Klein <bklein261@gmail.com>
2018-08-19 21:23:16 -0400

    bgimg binder

--- site/pages/class/class.js
+++ site/pages/class/class.js
@@ -4,7 +4,7 @@ ctrl = {
 
 $(document).ready(function() {
 
-  rivets.binders['bgimg'] = function(el, value){ el.style.setProperty("BackgroundImage", value); };
+  rivets.binders['bgimg'] = function(el, value){ el.style.setProperty("background", "url('" + value + "')" ); };
   rivets.bind( document.body, { data: data, ctrl: ctrl })
 
 })
