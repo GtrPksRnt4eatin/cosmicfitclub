457778ffd970a5a7a152e2d5bae27318122d56d9
Maxwell Klein <bklein261@gmail.com>
2018-08-22 03:21:12 -0400

    banner

--- site/pages/index/index.js
+++ site/pages/index/index.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
 
-  $.get('/models/slides', function(slides) {
-    slides = JSON.parse(slides);
-    slides.forEach( function(slide) {
-      $('#imgscroller').slick('slickAdd',`<div><img alt='' class='sliderimg' src='${ slide.url }' /></div>`);
-    }); 
-  });
+//  $.get('/models/slides', function(slides) {
+//    slides = JSON.parse(slides);
+//    slides.forEach( function(slide) {
+//      $('#imgscroller').slick('slickAdd',`<div><img alt='' class='sliderimg' src='${ slide.url }' /></div>`);
+//    }); 
+//  });
 
-  $('#imgscroller').slick({
-    variableHeight: true,
-  	centerMode: true,
-  	centerPadding: '60px',
-  	variableWidth: true,
-  	infinite: true,
-  	autoplay: true,
-    autoplaySpeed: 2000,
-    slidesToShow: 1
-  });
+//  $('#imgscroller').slick({
+//    variableHeight: true,
+//  	centerMode: true,
+//  	centerPadding: '60px',
+//  	variableWidth: true,
+//  	infinite: true,
+//  	autoplay: true,
+//    autoplaySpeed: 2000,
+//    slidesToShow: 1
+ // });
 
   //images.forEach( function(path) { var elem =  $('#imgscroller').slick('slickAdd',`<div><img class='sliderimg' src='${ path }' /></div>`); });
 
