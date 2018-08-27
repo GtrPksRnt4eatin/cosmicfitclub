a95f0ef6e2c7cd49de2b2419d6e97d67a6976c6c
Maxwell Klein <bklein261@gmail.com>
2018-08-26 23:31:54 -0400

    frontpage

--- site/pages/index/index.js
+++ site/pages/index/index.js
@@ -18,14 +18,14 @@ $(document).ready(function() {
   	if(data.i>=data.images.length) { data.i = 0; }
   	transition_img(data.images[data.i])
   	data.i = data.i + 1;
-  }, 1000 );  
+  }, 5000 );  
 
 });
 
 function transition_img(path) {
   console.log("Loading: " + path)
-  data.elements[1].attr('src')
-  data.elements[0].addClass('transparent')
-  data.elements[1].removeClass('transparent')
+  data.elements[1].attr('src', path);
+  data.elements[0].addClass('transparent');
+  data.elements[1].removeClass('transparent');
   data.elements.push(data.elements.shift());
 }
