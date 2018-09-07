1a0613f0962c00ba0ae9e0f2f16047286561d6e5
Maxwell Klein <bklein261@gmail.com>
2018-09-06 22:41:50 -0400

    imgfader work

--- site/pages/index/index.js
+++ site/pages/index/index.js
@@ -1,10 +1,10 @@
 data = {
   i: 0,
   images: [
-    { "path": "/wide_handstand_1920.png",  "msg": "Learn How To Handstand!" },
     { "path": "/wide_freeclass_1920.png",  "msg": "Get Your First Class Free!" },
     { "path": "/wide_freeevents_1920.png", "msg": "Check Out Our Free Events!" },
-    { "path": "/wide_membership_1920.png", "msg": "Join The Club! Become a Member Today!" }
+    { "path": "/wide_membership_1920.png", "msg": "Join The Club! Become a Member Today!" },
+    { "path": "/wide_handstand_1920.png",  "msg": "Learn How To Handstand!" }
   ],
   elements: []
 }
@@ -25,18 +25,24 @@ $(document).ready(function() {
 
   setInterval( function() {
   	if(data.i>=data.images.length) { data.i = 0; }
-  	transition_img(data.images[data.i]["path"])
-  	set_msg(data.images[data.i]["msg"])
+  	transition_img(data.images[data.i]["path"]);
+  	set_msg(data.images[data.i]["msg"]);
   	data.i = data.i + 1;
   }, 7000 );
+
+  $('.img1, .img2').load(function(e){
+    data.elements[0].addClass('transparent');
+    data.elements[1].removeClass('transparent');
+    data.elements.push(data.elements.shift());
+  });
 
 });
 
 function transition_img(path) {
   data.elements[1].attr('src', path);
-  data.elements[0].addClass('transparent');
-  data.elements[1].removeClass('transparent');
-  data.elements.push(data.elements.shift());
+  //data.elements[0].addClass('transparent');
+  //data.elements[1].removeClass('transparent');
+  //data.elements.push(data.elements.shift());
 }
 
 function set_msg(msg) {
