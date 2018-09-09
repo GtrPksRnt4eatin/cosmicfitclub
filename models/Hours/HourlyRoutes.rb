4965d2506e678b73e3c716f58b4b66b7f64c64d2
Maxwell Klein <bklein261@gmail.com>
2018-09-08 20:43:20 -0400

    customer id check

--- models/Hours/HourlyRoutes.rb
+++ models/Hours/HourlyRoutes.rb
@@ -5,14 +5,13 @@ class HourlyRoutes < Sinatra::Base
   end
 
   get '/punches' do
-    p params[ :customer_id ]
+    custy = Customer[params[:customer_id]] or halt(404, "Cant Find Customer")
     p HourlyPunch.where( :customer_id => params[ :customer_id ].to_i )
     HourlyPunch.where( :customer_id => params[ :customer_id ].to_i ).to_json
   end
 
   post '/punch_in' do
-    custy = Customer[params[:id]] or halt(404, "Cant Find Customer")
-    
+    custy = Customer[params[:customer_id]] or halt(404, "Cant Find Customer")
   	HourlyPunch.create( 
   	  :customer_id => custy.id, 
   	  :hourly_task_id => params[:hourly_task_id],
