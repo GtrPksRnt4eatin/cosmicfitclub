a3c1d93544e8d1b4f796a9f078ed9a3da8fcbd0b
Maxwell Klein <bklein261@gmail.com>
2018-09-08 21:56:42 -0400

    duration seconds

--- user/pages/hourly/hourly.js
+++ user/pages/hourly/hourly.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
   include_rivets_dates();
 
   rivets.formatters.task_name = function(val)          { task = data['hourly_tasks'].find( function(x) { return x.id == val } ); return ( task ? task.name : "" ) }
-  rivets.formatters.elapsed_time = function(val,start) { var dur = moment.duration(val.diff(start)); return(dur.hours() + ':' + dur.minutes()); }
+  rivets.formatters.elapsed_time = function(val,start) { var dur = moment.duration(val.diff(start)); return(dur.hours() + 'Hours ' + dur.minutes() + 'Minutes' + dur.seconds() + 'Seconds'); }
   rivets.bind( document.body, { data: data } );
 
   get_punches();
