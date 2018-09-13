03ab7abbbaf064dfed494bb4dd45332a463a4865
Maxwell Klein <bklein261@gmail.com>
2018-09-13 00:49:23 -0400

    punches to_h

--- models/Hours/HourlyRoutes.rb
+++ models/Hours/HourlyRoutes.rb
@@ -14,7 +14,9 @@ class HourlyRoutes < Sinatra::Base
   get '/my_punches' do
     content_type :json
     custy = session[:customer]                  or halt(401, "Not Signed In")
-    HourlyPunch::punches(custy.id).to_h.to_json
+    punches = HourlyPunch::punches(custy.id)
+    p punches
+    punches.to_json
   end
 
   post '/punch_in' do
