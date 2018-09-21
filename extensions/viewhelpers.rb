393bbee854e6816ceeb8c5c1d400fbc4f1db3cda
Maxwell Klein <bklein261@gmail.com>
2018-09-20 21:23:42 -0400

    hotjar

--- extensions/viewhelpers.rb
+++ extensions/viewhelpers.rb
@@ -26,7 +26,7 @@ module Sinatra
     def fb_description(description) "\n<meta property='og:description' content='#{description}' />" end 
 
     def analytics
-      <<-eos
+      hotjar + <<-eos
         <script>
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -38,7 +38,23 @@ module Sinatra
         </script>
       eos
     end
-    
+
+    def hotjar
+      <<-eos
+        <!-- Hotjar Tracking Code for cosmicfitclub.com -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:1022582,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
+      eos
+    end
+
   end
 
 end
