200cbe2c21337429d800558075a55acac5bbafff
Maxwell Klein <bklein261@gmail.com>
2018-10-06 04:06:58 -0400

    occurrences

--- models/Classes/ClassDef.rb
+++ models/Classes/ClassDef.rb
@@ -38,7 +38,7 @@ class ClassDef < Sequel::Model
 
   def get_full_occurences(from, to)
     schedules.map do |sched|
-      sched.get_occurences(from,to).map do |x|
+      sched.get_occurrences(from,to).map do |x|
         exception = ClassException.find( :classdef_id => self.id, :original_starttime => x.start_time.iso8601 )
         teachers = ( exception.nil? ? sched.teachers : exception[:teacher_id].nil? ? sched.teachers : [ Staff[exception[:teacher_id]] ] )
         { :teachers => teachers.map { |x| { :id => x[:id], :name => x[:name], :image_url => x.image_url(:small) } }, 
