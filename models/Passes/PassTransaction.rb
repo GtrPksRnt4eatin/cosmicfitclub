fb8ba3e716eb0f665dbd6c54ecb09437798677b1
Maxwell Klein <bklein261@gmail.com>
2018-10-06 17:57:29 -0400

    comptickets

--- models/Passes/PassTransaction.rb
+++ models/Passes/PassTransaction.rb
@@ -2,6 +2,7 @@ class PassTransaction < Sequel::Model
 
   many_to_one :reservation, :class => :ClassReservation, :id => :reservation_id
   many_to_one :wallet
+  one_to_many :comptickets
 
   def before_create
     self.timestamp = Time.now
