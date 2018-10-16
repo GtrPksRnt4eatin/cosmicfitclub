acc3a5fd2ba923a887fda39d40141f1adf08ca61
Maxwell Klein <bklein261@gmail.com>
2018-10-16 04:09:29 -0400

    ticket names

--- models/Events/Event.rb
+++ models/Events/Event.rb
@@ -95,11 +95,12 @@ class Event < Sequel::Model
         id = tic.customer ? tic.customer.id : 0
         name = tic.customer ? tic.customer.name : ""
         email = tic.customer ? tic.customer.email : ""
-        csv << [ id, name, email, "$ 0.00", "$ 0.00", "$0.00", "$ 0.00" ] unless trans
-        csv << ( [ id, name, email ] + [trans.amount, trans.fee, refund, (trans.net + refund) ].map(&:fmt_stripe_money) ) if trans
+        csv << [ id, name, email, "$ 0.00", "$ 0.00", "$0.00", "$ 0.00", tic.price.name ] unless trans
+        csv << ( [ id, name, email ] + [trans.amount, trans.fee, refund, (trans.net + refund) ].map(&:fmt_stripe_money) + [tic.price.name] ) if trans
       end 
       csv << []
       csv << [ "Totals:", self.headcount, "" ] + [ gross, fees, refunds, net ].map(&:fmt_stripe_money)
+      csv.read
     end
   end
 
