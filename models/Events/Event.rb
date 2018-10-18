b600970d58a2d0cedc676d2be87e014a7abecd52
Maxwell Klein <bklein261@gmail.com>
2018-10-18 06:31:37 -0400

    cash

--- models/Events/Event.rb
+++ models/Events/Event.rb
@@ -131,16 +131,19 @@ class Event < Sequel::Model
         custy        = tic.customer_info
         custy_info   = [ custy[:id], custy[:name], custy[:email] ]
 
-        if used_payment_ids.include?(tic.stripe_payment_id)
-          payment_info = [ 0, 0, 0, 0 ].map(&:fmt_stripe_money)
+        if tic.stripe_payment_id.nil? then
+          payment_info = [ tic.price, 0, 0, tic.price ].map(&:fmt_stripe_money)
+          totals.merge!({ :gross => tic.price, :net => tic.price }) { |key, v1, v2| v1 + v2 }
         else
-          used_payment_ids << tic.stripe_payment_id
-          payment      = tic.full_payment_info
-          totals.merge!(payment) { |key, v1, v2| v1 + v2 } unless payment.nil?
-          payment_info = [ payment[:gross], payment[:fees], payment[:refunds], payment[:net] ].map(&:fmt_stripe_money)
+          if used_payment_ids.include?(tic.stripe_payment_id) then
+            payment_info = [ 0, 0, 0, 0 ].map(&:fmt_stripe_money)
+          else
+            used_payment_ids << tic.stripe_payment_id
+            payment      = tic.full_payment_info
+            totals.merge!(payment) { |key, v1, v2| v1 + v2 } unless payment.nil?
+            payment_info = [ payment[:gross], payment[:fees], payment[:refunds], payment[:net] ].map(&:fmt_stripe_money)
+          end
         end
-
-        payment_info = [ tic.price, 0, 0, tic.price ].map(&:fmt_stripe_money) if tic.stripe_payment_id.nil?
         
         [ tic.id, tic.created_on.strftime("%a %m/%d %I:%M %P") ] + custy_info + payment_info + [ tic.eventprice.try(:title), tic.recipient.try(:id), tic.recipient.try(:name), tic.recipient.try(:email) ]
       
