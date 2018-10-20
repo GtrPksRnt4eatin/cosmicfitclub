216409801754c2924fd16b51bf34177dc64e77c9
Maxwell Klein <bklein261@gmail.com>
2018-10-19 23:17:35 -0400

    google scope

--- ruby/services/sheets.rb
+++ ruby/services/sheets.rb
@@ -17,21 +17,23 @@ module Sheets
     token_store = Google::Auth::Stores::FileTokenStore.new(file: CREDENTIALS_PATH)
     authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
     user_id = 'default'
-    credentials = authorizer.get_credentials(user_id)
-    if credentials.nil?
-      url = authorizer.get_authorization_url(base_url: OOB_URI)
-      puts "Open the following URL in the browser and enter the resulting code after authorization"
-      puts url
-      code = gets
-      credentials = authorizer.get_and_store_credentials_from_code(user_id: user_id, code: code, base_url: OOB_URI)
-    end
-    credentials
+    authorizer.get_credentials(user_id)
+    #credentials = authorizer.get_credentials(user_id)
+    #return nil if credentials.nil?
+    #  url = authorizer.get_authorization_url(base_url: OOB_URI)
+    #  puts "Open the following URL in the browser and enter the resulting code after authorization"
+    #  puts url
+    #  code = gets
+    #  credentials = authorizer.get_and_store_credentials_from_code(user_id: user_id, code: code, base_url: OOB_URI)
+    #end
+    #credentials
   end
 
   def Sheets.read_sheet
     sheets = Google::Apis::SheetsV4::SheetsService.new
     sheets.client_options.application_name = APPLICATION_NAME
     sheets.authorization = Sheets.authorize
+    return if sheets.authorization.nil?
     spreadsheet_id = '1XckfZT_IRPZ43VCadw8yiQzUN4Xv5eh8IWbFI01ZLKE'
     range = 'A2:B30'
     response = sheets.get_spreadsheet_values(spreadsheet_id, range)
