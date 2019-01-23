04b454a5950ca180c4099a54954e37c956bcdb5e
Maxwell Klein <bklein261@gmail.com>
2019-01-23 01:10:19 -0500

    payroll page

--- admin/pages/payroll/payroll.js
+++ admin/pages/payroll/payroll.js
@@ -1,10 +1,17 @@
 data = {
-  range: ""
+  range: "",
+  payroll: []
 }
 
 ctrl = {
   rangeselect: function(e,m) {
     var x = 5;
+  },
+
+  get_data: function(e,m) {
+  	var match = /(\d{4}-\d{2}-\d{2}) to (\d{4}-\d{2}-\d{2})/.exec(data['range']);
+  	if(!match) { return; }
+    $.get('/models/staff/payroll', { from: match[1], to: match[2] }, on_payroll_data);
   }
 }
 
@@ -12,3 +19,7 @@ $(document).ready(function() {
   include_rivets_dates();
   rivets.bind( document.body, { data: data, ctrl: ctrl } );
 });
+
+function on_payroll_data(resp) {
+  data.payroll = resp;
+}
