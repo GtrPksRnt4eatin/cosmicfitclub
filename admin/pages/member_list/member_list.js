ce6aaaea2552081955e8a52e0bfb337b78b38379
Maxwell Klein <bklein261@gmail.com>
2019-02-27 19:31:38 -0500

    added route

--- admin/pages/member_list/member_list.js
+++ admin/pages/member_list/member_list.js
@@ -1,7 +1,20 @@
+data = {
+  subscriptions: {}
+}
+
 ctrl = {
 	
 }
 
 $(document).ready(function(){
   rivets.bind(document.body, { data: data, ctrl: ctrl } );
+  get_data();
 });
+
+function get_data() {
+  $.get("/models/memberships/grouped_list", on_list )
+}
+
+function on_list(resp) {
+  data['subscriptions'] = resp;
+}
