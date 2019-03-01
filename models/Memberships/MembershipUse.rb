30e952f02c6440635ce675ff9b9be621fdb29862
Maxwell Klein <bklein261@gmail.com>
2019-03-01 00:02:10 -0500

    subscription list

--- models/Memberships/MembershipUse.rb
+++ models/Memberships/MembershipUse.rb
@@ -1,14 +1,15 @@
 class MembershipUse < Sequel::Model
 
-  many_to_one :reservation, :class=>:ClassReservation, :key=>:reservation_id
-  many_to_one :membership,  :class=>:Subscription, :key=>:subscription_id
+  many_to_one :reservation,  :class=>:ClassReservation, :key=>:reservation_id
+  many_to_one :membership,   :class=>:Subscription,     :key=>:subscription_id
+  many_to_one :subscription, :class=>:Subscription,     :key=>:subscription_id
 
   def undo
   	self.delete
   end
 
   def employee_discount?
-    self.membership.plan.id == 10
+    self.subscription.plan.id == 10
   end
 
 end
