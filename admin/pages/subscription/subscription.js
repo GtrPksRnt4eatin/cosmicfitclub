7c79c5efc35ea97b81ccf407a8240a92994049bf
Maxwell Klein <bklein261@gmail.com>
2019-03-02 01:54:19 -0500

    center

--- admin/pages/subscription/subscription.js
+++ admin/pages/subscription/subscription.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
   rivets.formatters.remove_invalid = function(val) { return val == "Invalid date" ? '' : val; }
   rivets.formatters.subscription_url = function(val) { return "https://dashboard.stripe.com/subscriptions/" + val; }
   rivets.formatters.customer_url = function(val) { return "/frontdesk/customer_file?id=" + val; }
+  rivets.formatters.count = function(val) { return val.length; }
   rivets.bind(document.body, { data: data, ctrl: ctrl } );
   get_data();
 });
