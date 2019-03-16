0ad4189caf06c2887f4eb97097ecc27b27a59501
Maxwell Klein <bklein261@gmail.com>
2019-03-16 19:32:53 -0400

    selectize

--- frontdesk/pages/class_attendance/class_attendance.js
+++ frontdesk/pages/class_attendance/class_attendance.js
@@ -68,8 +68,8 @@ $(document).ready( function() {
     payment_form.ev_sub('hide', popupmenu.hide );
     popupmenu.ev_sub('close', payment_form.stop_listen_cardswipe);
 
-    $('#customers').chosen({ search_contains: true });
-    $('#customers').on('change', reservation_form.load_customer );
+    //$('#customers').chosen({ search_contains: true });
+    //$('#customers').on('change', reservation_form.load_customer );
 
 });
 
