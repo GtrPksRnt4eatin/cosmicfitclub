c970bb285ddf40b1cfcd3b28efd085c3b51e1667
Maxwell Klein <bklein261@gmail.com>
2019-04-02 02:27:40 -0400

    rescue me

--- models/Schedule/ScheduleRoutes.rb
+++ models/Schedule/ScheduleRoutes.rb
@@ -21,7 +21,7 @@ class ScheduleRoutes < Sinatra::Base
       items.each do |day,item|
         csv << ['',day,'']
         item.each do |line|
-          csv << [ line['starttime'].try(:strftime,'%l:%M %p'), line['endtime'].try(:strftime,'%l:%M %p'), line['title'], line['instructors'].map(&name).join , line.to_json ]
+          csv << [ line['starttime'].try(:strftime,'%l:%M %p'), line['endtime'].try(:strftime,'%l:%M %p'), line['title'], line['instructors'], line.to_json ]
         end
       end
     end
