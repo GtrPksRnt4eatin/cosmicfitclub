ed7035e416601fbb6837726ad992412d5c4eb0b8
Maxwell Klein <bklein261@gmail.com>
2019-04-02 03:04:25 -0400

    parse date

--- models/Schedule/ScheduleRoutes.rb
+++ models/Schedule/ScheduleRoutes.rb
@@ -19,7 +19,7 @@ class ScheduleRoutes < Sinatra::Base
       csv << ['COSMIC FIT CLUB SCHEDULE', from.to_s, to.to_s]
       csv << []
       items.each do |day,item|
-        csv << ['', day.strftime('%A %b %e') ,'']
+        csv << ['', Date.parse(day).strftime('%A %b %e') ,'']
         item.each do |line|
           csv << ["#{line[:starttime].strftime('%l:%M %p')} - #{line[:endtime].strftime('%l:%M %p')}", line[:title], line[:instructors].try(:map,&:name).try(:join), line.to_json ]
         end
