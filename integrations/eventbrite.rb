f3c770f997c7d07f916a6213b7ffd8ee91314b65
Maxwell Klein <bklein261@gmail.com>
2019-04-02 01:40:58 -0400

    add event name

--- integrations/eventbrite.rb
+++ integrations/eventbrite.rb
@@ -12,11 +12,14 @@ class EventBriteRoutes < Sinatra::Base
   	case event['config']['action']
      
     when 'order.placed'
-      Slack.custom("EventBrite Order Placed:\r\revent_id: #{event["api_object"]["event_id"]}\rname: #{event["api_object"]["name"]}\remail: #{event["api_object"]["email"]}")
+      ename = EventbriteSDK::get({ :url => "events/#{event['api_object']['event_id']}" })['name']['text']
+      Slack.custom("EventBrite Order Placed:\r\revent_id: #{event["api_object"]["event_id"]}\revent_name: #{ename}\rname: #{event["api_object"]["name"]}\remail: #{event["api_object"]["email"]}")
       ( Slack.custom("But Customer Already Exists!"); return ) if Customer::exists? event["api_object"]["email"]
       custy = Customer.get_from_email( event["api_object"]["email"], event["api_object"]["name"] )
     when 'event.updated'
       Slack.post("EventBrite Event Updated: #{event['api_object']['name']['text']}")
+    when 'ticket_class.updated'
+
     else
       Slack.webhook('EventBrite Webhook: ', JSON.pretty_generate(event) )
     end
