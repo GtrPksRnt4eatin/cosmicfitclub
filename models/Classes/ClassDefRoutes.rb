2388c2b95fea5ec8a950bf3f1494ae8aef6ea572
Maxwell Klein <bklein261@gmail.com>
2019-04-17 19:04:52 -0400

    custy.id

--- models/Classes/ClassDefRoutes.rb
+++ models/Classes/ClassDefRoutes.rb
@@ -136,7 +136,7 @@ class ClassDefRoutes < Sinatra::Base
     halt 400 if custy.nil?
     occurrence = ClassOccurrence.get(params[:classdef_id], params[:staff_id], params[:starttime])
     !occurrence.full?                         or halt(409, "Class is Full")
-    !occurrence.has_reservation_for? custy_id or halt(409, "This Person is Already Checked In") 
+    !occurrence.has_reservation_for? custy.id or halt(409, "This Person is Already Checked In") 
     message = "#{custy.name} Registered for #{ClassDef[params[:classdef_id]].name} with #{Staff[params[:staff_id]].name} on #{params[:starttime]}"    
     case params[:transaction_type]
     when "class_pass"
