4c1dfec990f9eb17cc6aaaa76bd49a39809751d4
Maxwell Klein <bklein261@gmail.com>
2019-05-07 03:58:13 -0400

    new staff page

--- admin/pages/staff_detail/staff_detail.js
+++ admin/pages/staff_detail/staff_detail.js
@@ -5,6 +5,10 @@ var ctrl = {}
 $(document).ready(function() {
 
   rivets.formatters.subscription_link = function(val) { return '/admin/subscription?id=' + val; }
+  
+  include_rivets_dates();
+
+  rivets.formatters.count = function(val) { return val.length; }
 
   rivets.bind(document.body, { data: data, ctrl: ctrl } );
 
