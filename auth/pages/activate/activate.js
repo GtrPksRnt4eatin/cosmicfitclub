c24da8351b130176ce141b3c548384406fc7d196
Maxwell Klein <bklein261@gmail.com>
2019-05-10 17:26:55 -0400

    activate

--- auth/pages/activate/activate.js
+++ auth/pages/activate/activate.js
@@ -7,7 +7,7 @@ $(document).ready( function() {
         type:'post',
         data:$('#myform').serialize(),
         success:function() { window.location.href = '/user';  },
-        error: function(resp)  { $('.tile').shake(); $('.errors').innerText=resp.responseText; }
+        error: function(resp)  { $('.tile').shake(); $('.errors')[0].innerText=resp.responseText; }
       });
     });
 
