d51949972214df6a5282a465c6d28b042d78d4e9
Maxwell Klein <bklein261@gmail.com>
2019-05-13 02:46:01 -0400

    payroll page

--- admin/pages/payroll/payroll.js
+++ admin/pages/payroll/payroll.js
@@ -18,6 +18,8 @@ ctrl = {
 
 $(document).ready(function() {
   include_rivets_dates();
+  include_rivets_money();
+  rivets.formatters.upcase = function(val) { return val.toUpperCase(); }
   rivets.bind( document.body, { data: data, ctrl: ctrl } );
 });
 
