6c7fa4b3cb6d7fba7a2a3084a1a1b5db563b25ba
Maxwell Klein <bklein261@gmail.com>
2019-05-29 11:29:38 -0400

    event caching

--- checkout/pages/event/event.js
+++ checkout/pages/event/event.js
@@ -61,22 +61,22 @@ function get_event_data() {
 
 function set_event_mode() {
   
-  if( EVENT.registration_url          ) { data.mode = 'external';   return; }
-  if( EVENT.a_la_carte                ) { data.mode = 'a_la_carte'; return; }
-  if( EVENT.prices.length>1           ) { data.mode = 'multi';      return; }
+  if( data.event_data.registration_url          ) { data.mode = 'external';   return; }
+  if( data.event_data.a_la_carte                ) { data.mode = 'a_la_carte'; return; }
+  if( data.event_data.prices.length>1           ) { data.mode = 'multi';      return; }
   if( free_event()                    ) { data.mode = 'free';       return; }
-  if( EVENT.prices[0].member_price==0 ) { data.mode = 'memberfree'; return; }
-  if( EVENT.prices.length == 1        ) { data.mode = 'single';     return; }
+  if( data.event_data.prices[0].member_price==0 ) { data.mode = 'memberfree'; return; }
+  if( data.event_data.prices.length == 1        ) { data.mode = 'single';     return; }
 
 }
 
 function set_first_price() {
   
   clear_selected_price();
-  if(empty(EVENT.prices[0])) return;
-  EVENT.prices[0].selected = true;
-  data.selected_price = EVENT.prices[0];
-  set_included_sessions(EVENT.prices[0].included_sessions);
+  if(empty(data.event_data.prices[0])) return;
+  data.event_data.prices[0].selected = true;
+  data.selected_price = data.event_data.prices[0];
+  set_included_sessions(data.event_data.prices[0].included_sessions);
   calculate_total();
 
 }
@@ -91,7 +91,7 @@ function calculate_total() {
 
     case 'memberfree':
     case 'single':
-      data.total_price = ( member() ? EVENT.prices[0].member_price : EVENT.prices[0].full_price ); 
+      data.total_price = ( member() ? data.event_data.prices[0].member_price : data.event_data.prices[0].full_price ); 
       break;
 
     case 'multi':
@@ -125,7 +125,7 @@ function calculate_custom_prices() {
   data.custom_full_price = 0;
   data.custom_member_price = 0;
   for(var i=0; i<data.included_sessions.length; i++) {
-    sess = EVENT.sessions.find( function(x) { return x.id == data.included_sessions[i]; } );
+    sess = data.event_data.sessions.find( function(x) { return x.id == data.included_sessions[i]; } );
     data.custom_full_price += sess.individual_price_full;
     data.custom_member_price += sess.individual_price_member;
   } 
@@ -133,8 +133,8 @@ function calculate_custom_prices() {
 }
 
 function set_included_sessions(sessions) {
-  for(var i=0; i<EVENT.sessions.length; i++) { 
-    EVENT.sessions[i].selected = sessions.indexOf(EVENT.sessions[i].id)!=-1;
+  for(var i=0; i<data.event_data.sessions.length; i++) { 
+    data.event_data.sessions[i].selected = sessions.indexOf(data.event_data.sessions[i].id)!=-1;
   }
   data.included_sessions = sessions.slice(0);
 }
@@ -149,10 +149,10 @@ function toggle_included_session(session) {
 }
 
 function clear_selected_price() {
-  for(var i=0; i<EVENT.prices.length; i++)   { EVENT.prices[i].selected = false; }
+  for(var i=0; i<data.event_data.prices.length; i++)   { data.event_data.prices[i].selected = false; }
 }
 
-function free_event() { return EVENT.prices[0].member_price==0 && EVENT.prices[0].full_price==0 }
+function free_event() { return data.event_data.prices[0].member_price==0 && data.event_data.prices[0].full_price==0 }
 
 function signed_in()  { return !empty(CUSTOMER); }
 function member()     { return signed_in() ? !empty(CUSTOMER.plan) : false; }
@@ -196,7 +196,7 @@ function checkout() {
 
   STRIPE_HANDLER.open({
     name: 'Cosmic Fit Club',
-    description: EVENT['title'],
+    description: data.event_data['title'],
     image: 'https://cosmicfit.herokuapp.com/background-blu.jpg',
     amount: data.total_price
   });
@@ -206,7 +206,7 @@ function checkout() {
 function register() {
 
   body = JSON.stringify({
-    "event_id": EVENT['id'],
+    "event_id": data.event_data['id'],
     "included_sessions": data.included_sessions,
     "email": signed_in() ? CUSTOMER.email : id('email').value
   });
@@ -221,11 +221,11 @@ function on_token_received(token) {
 
   body = JSON.stringify({ 
     "type":  "event", 
-    "event_id": EVENT['id'], 
+    "event_id": data.event_data['id'], 
     "total_price": data.total_price,
     "included_sessions": data.included_sessions,
     "metadata": {
-      "event_id": EVENT['id'], 
+      "event_id": data.event_data['id'], 
       "included_sessions": data.included_sessions.join(','),
       "selected_price": empty(data.selected_price) ? 0 : data.selected_price.id
     },
