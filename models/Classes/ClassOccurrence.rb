d95b78f701d22970e354f8cd636ea61eea4d799e
Maxwell Klein <bklein261@gmail.com>
2019-07-11 02:05:49 -0400

    prev & next occurrence

--- models/Classes/ClassOccurrence.rb
+++ models/Classes/ClassOccurrence.rb
@@ -53,11 +53,15 @@ class ClassOccurrence < Sequel::Model
 
   def next_occurrence_id
     p [self.classdef_id, self.staff_id, self.starttime, self.starttime]
+    p $DB[ClassOccurrence.next_query, self.classdef_id, self.staff_id, self.starttime, self.starttime].sql
     p $DB[ClassOccurrence.next_query, self.classdef_id, self.staff_id, self.starttime, self.starttime].first
     $DB[ClassOccurrence.next_query, self.classdef_id, self.staff_id, self.starttime, self.starttime].first.try(:id)
   end
 
   def previous_occurrence_id
+    p [self.classdef_id, self.staff_id, self.starttime, self.starttime]
+    p $DB[ClassOccurrence.previous_query, self.classdef_id, self.staff_id, self.starttime, self.starttime].sql
+    p $DB[ClassOccurrence.previous_query, self.classdef_id, self.staff_id, self.starttime, self.starttime].first
     $DB[ClassOccurrence.previous_query, self.classdef_id, self.staff_id, self.starttime, self.starttime].first.try(:id)
   end
 
