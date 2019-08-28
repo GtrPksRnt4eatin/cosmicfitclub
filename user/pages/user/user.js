9d8db0f35c442e11e19a565d4d1ba7b3f7557b46
Benjamin Maxwell <bklein261@gmail.com>
2019-08-28 16:21:29 -0400

    cancel reservation

--- user/pages/user/user.js
+++ user/pages/user/user.js
@@ -1,7 +1,15 @@
+ctrl = {
+  cancel_res: function(e,m) {
+  	if( !confirm("Really Cancel Your Reservation?") ) return;
+    $.del('/models/classdefs/reservations/' + m.res.id)
+     .success( function() { location.reload(); } );
+  }
+}
+
 $(document).ready(function() {
 
   userview = new UserView( id('userview_container'));
 
-  rivets.bind({ data: data })
+  rivets.bind({ data: data, ctrl: ctrl })
 
 });
