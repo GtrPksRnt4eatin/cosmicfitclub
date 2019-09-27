256ac8906310f6fe4efd09a4eb879cc8803ac4c2
Maxwell Klein <bklein261@gmail.com>
2019-09-27 00:07:34 -0400

    event checkout

--- ruby/db_models.rb
+++
@@ -1,6 +0,0 @@
-require_relative './environment'
-require_relative '../integrations/database'
-require_relative 'ruby/shrine'
-
-Dir["models/mixins/*.rb"].each { |file| require_relative file }
-Dir["models/**/*.rb"].each     { |file| require_relative file }
---
+++ utillities/db_models.rb
@@ -0,0 +1,6 @@
+require_relative '../ruby/environment'
+require_relative '../integrations/database'
+require_relative '../ruby/shrine'
+
+Dir["../models/mixins/*.rb"].each { |file| require_relative file }
+Dir["../models/**/*.rb"].each     { |file| require_relative file }
