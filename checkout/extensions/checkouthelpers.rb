c81e9cbefa52ff9f5c72de90759d98ca54e32e36
Maxwell Klein <bklein261@gmail.com>
2019-10-04 01:45:28 -0400

    multiple tickets

--- checkout/extensions/checkouthelpers.rb
+++ checkout/extensions/checkouthelpers.rb
@@ -40,23 +40,25 @@ module Sinatra
       custy.send_new_account_email if custy.login.nil?
       eventname = Event[data['metadata']['event_id']].name
       data['metadata']['name'] = data['token']['card']['name']
-      charge = StripeMethods::charge_card(data['token']['id'], data['total_price'], data['token']['email'], eventname, data['metadata']);
-      params['multiplier'] ||= 1
+    
+      data['multiplier'] ||= 1
 
-      if params['multiplier'] > 1 then
-        description = "[\##{custy.id}] #{custy.name} (#{custy.email}) bought #{params['multiplier']} $#{data['total_price']/params['multiplier']/100} tickets for #{eventname}."
+      if data['multiplier'] > 1 then
+        description = "[\##{custy.id}] #{custy.name} (#{custy.email}) bought #{data['multiplier']} $#{data['total_price']/data['multiplier'].to_i/100} tickets for #{eventname}."
       else
         description = "[\##{custy.id}] #{custy.name} (#{custy.email}) bought a $#{data['total_price']/100} ticket for #{eventname}."
       end
 
+      charge = StripeMethods::charge_card(data['token']['id'], data['total_price'], data['token']['email'], description, data['metadata']);
+
       payment = CustomerPayment.create(:customer => custy, :stripe_id => charge.id, :amount => data['total_price'], :reason => description, :type => 'new card')
       
-      params['multiplier'].to_i.times do 
+      data['multiplier'].to_i.times do 
         EventTicket.create( 
           :customer            => custy, 
           :event_id            => data['event_id'], 
           :included_sessions   => data['included_sessions'], 
-          :price               => data['total_price'] / params['multiplier'],
+          :price               => data['total_price'] / data['multiplier'].to_i,
           :event_price_id      => data['selected_price'] ? data['selected_price']['id'] : nil,
           :customer_payment_id => payment.id
         )
@@ -72,7 +74,7 @@ module Sinatra
           :customer_id         => params[:customer_id],
           :event_id            => params[:event_id],
           :included_sessions   => params[:included_sessions],
-          :price               => params[:total_price] / params[:multiplier],
+          :price               => params[:total_price] / params[:multiplier].to_i,
           :customer_payment_id => params[:payment_id], 
           :event_price_id      => params[:price_id]
         )
