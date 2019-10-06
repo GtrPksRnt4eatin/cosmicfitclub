c6d3e97e6d4b8e778dd81deaf05c4f32c6f4d55b
Maxwell Klein <bklein261@gmail.com>
2019-10-06 19:43:45 -0400

    schedule jpg

--- models/Schedule/SchedulePoster.rb
+++ models/Schedule/SchedulePoster.rb
@@ -3,7 +3,7 @@ require 'mini_magick'
 
 module SchedulePoster
  
-  def set_class_vars
+  def SchedulePoster::set_class_vars
     @@line_height      = 60
     @@header_pointsize = 55
     @@body_pointsize   = 42
@@ -14,8 +14,8 @@ module SchedulePoster
  
   def SchedulePoster::generate(starttime, high_contrast=false)
 
-  	set_class_vars
-    
+  	SchedulePoster::set_class_vars
+
   	@@high_contrast = high_contrast
     @@schedule      = Scheduling::get_all_sorted_by_days(starttime, starttime >> 7)
     
@@ -25,13 +25,13 @@ module SchedulePoster
     @@image.rotate "90"
     @@image.resize "2550x3300!"
     
-    offset = build_day(0,50,150)
-    offset = build_day(1,50,offset + 25)
-    offset = build_day(2,50,offset + 25)
-    offset = build_day(3,50,offset + 25)
-    offset = build_day(4,50,offset + 25)
-    offset = build_day(5,50,offset + 25)
-    offset = build_day(6,50,offset + 25)
+    offset = SchedulePoster::build_day(0,50,150)
+    offset = SchedulePoster::build_day(1,50,offset + 25)
+    offset = SchedulePoster::build_day(2,50,offset + 25)
+    offset = SchedulePoster::build_day(3,50,offset + 25)
+    offset = SchedulePoster::build_day(4,50,offset + 25)
+    offset = SchedulePoster::build_day(5,50,offset + 25)
+    offset = SchedulePoster::build_day(6,50,offset + 25)
 
     @@image.to_blob
 
@@ -39,24 +39,24 @@ module SchedulePoster
 
   ################################### RENDERING ######################################
 
-  def draw_box(x_offset, y_offset, x, y, x_radius=25, y_radius=25 )
+  def SchedulePoster::draw_box(x_offset, y_offset, x, y, x_radius=25, y_radius=25 )
     @@image.combine_options do |i|
       i.fill "\#00000099"
       i.draw "roundRectangle #{x_offset},#{y_offset} #{x_offset + x},#{y_offset + y} #{x_radius},#{y_radius}"
     end
   end
 
-  def render_header_line(day, x_offset, y_offset)
+  def SchedulePoster::render_header_line(day, x_offset, y_offset)
     @@image.combine_options do |i|
       i.fill "\#FFFFFFFF" unless @@high_contrast
       i.fill "\#000000FF"     if @@high_contrast
       i.font "../shared/fonts/webfonts/329F99_3_0.ttf"
       i.pointsize @header_pointsize
-      i.draw "text #{x_offset},#{y_offset} '#{parse_day(day[:day])}'"
+      i.draw "text #{x_offset},#{y_offset} '#{SchedulePoster::parse_day(day[:day])}'"
     end
   end
 
-  def render_text_line(occ, x_offset, y_offset)
+  def SchedulePoster::render_text_line(occ, x_offset, y_offset)
     col1_x, col2_x, col3_x = 75, 600, 1750
     col2_trunc, col3_trunc = 53, 28
 
@@ -67,35 +67,35 @@ module SchedulePoster
       when 'classoccurrence'
         i.fill "\#FFFFFFFF" unless @@high_contrast
         i.fill "\#000000FF"     if @@high_contrast
-        i.draw "text #{x_offset + col1_x },#{y_offset} '#{line}'"
-        i.draw "text #{x_offset + col2_x },#{y_offset} '#{truncate(occ['title'],col2_trunc)}'"
-        i.draw "text #{x_offset + col3_x },#{y_offset} 'w/ #{truncate(occ['instructors'][0]['name'],col3_trunc)}'"
+        i.draw "text #{x_offset + col1_x },#{y_offset} '#{SchedulePoster::parse_time(occ)}'"
+        i.draw "text #{x_offset + col2_x },#{y_offset} '#{SchedulePoster::truncate(occ['title'],col2_trunc)}'"
+        i.draw "text #{x_offset + col3_x },#{y_offset} 'w/ #{SchedulePoster::truncate(occ['instructors'][0]['name'],col3_trunc)}'"
       when 'eventsession'
         i.fill "\#FFFFAAFF" unless @@high_contrast
         i.fill "\#999900FF"     if @@high_contrast
-        i.draw "text #{x_offset + col1_x },#{y_offset} '#{line}'"
-        i.draw "text #{x_offset + col2_x },#{y_offset} '#{truncate(occ['event_title'],col2_trunc)}'"
-        i.draw "text #{x_offset + col3_x },#{y_offset} '#{truncate(occ['title'],col3_trunc)}'"
+        i.draw "text #{x_offset + col1_x },#{y_offset} '#{SchedulePoster::parse_time(occ)}'"
+        i.draw "text #{x_offset + col2_x },#{y_offset} '#{SchedulePoster::truncate(occ['event_title'],col2_trunc)}'"
+        i.draw "text #{x_offset + col3_x },#{y_offset} '#{SchedulePoster::truncate(occ['title'],col3_trunc)}'"
       when 'private' then
         i.fill "\#AAAAFFFF" unless @@high_contrast
         i.fill "\#000099FF"     if @@high_contrast
-        i.draw "text #{x_offset + col1_x },#{y_offset} '#{line}'"
-        i.draw "text #{x_offset + col2_x },#{y_offset} '#{truncate(occ['title'],col2_trunc)}'"
+        i.draw "text #{x_offset + col1_x },#{y_offset} '#{SchedulePoster::parse_time(occ)}'"
+        i.draw "text #{x_offset + col2_x },#{y_offset} '#{SchedulePoster::truncate(occ['title'],col2_trunc)}'"
       end
     end
   end
 
-  def build_day(idx, x_offset, y_offset)
+  def SchedulePoster::build_day(idx, x_offset, y_offset)
     day = @@schedule[idx]
     box_height = (@@line_height * 1.7) + ( @@line_height * day[:occurrences].count )
-    draw_box(x_offset, y_offset, @@box_width, box_height ) unless @@high_contrast
+    SchedulePoster::draw_box(x_offset, y_offset, @@box_width, box_height ) unless @@high_contrast
     x_offset = x_offset + 20
     y_offset = y_offset + (@@line_height * 1.1)
-    render_header_line(day, x_offset, y_offset)
+    SchedulePoster::render_header_line(day, x_offset, y_offset)
     y_offset = y_offset + ( line_height * 0.1 )
     day[:occurrences].each do |occ|
       y_offset = y_offset + @@line_height
-      render_text_line(occ, x_offset, y_offset)
+      SchedulePoster::render_text_line(occ, x_offset, y_offset)
     end
     y_offset = y_offset + @@line_height*0.5
     return y_offset
@@ -105,17 +105,17 @@ module SchedulePoster
 
   #################################### HELPERS #######################################
 
-  def truncate(string,max)
+  def SchedulePoster::truncate(string,max)
     return "" if string.nil?
     string.length > max ? "#{string[0...max]}..." : string
   end
 
-  def parse_day(str)
+  def SchedulePoster::parse_day(str)
     dt = DateTime.parse(str)
     dt.strftime("%a %b %e")
   end 
 
-  def parse_time(occ)
+  def SchedulePoster::parse_time(occ)
     start   = occ[:starttime]
     finish  = occ[:endtime]
     start.strftime("%l:%M %P - ") + finish.strftime("%l:%M %P")
