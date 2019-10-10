99270d5e2f53793a1b86396847d906e329e4e394
Maxwell Klein <bklein261@gmail.com>
2019-10-10 11:53:24 -0400

    suckerpunch

--- background_jobs/BuildSchedulePoster.rb
+++ background_jobs/BuildSchedulePoster.rb
@@ -1,9 +1,11 @@
+require 'suckerpunch'
+
 class BuildSchedulePoster
   include SuckerPunch::Job
 
   def perform(starttime)
     blob = SchedulePoster::generate(starttime)
-    StoredImage.create( :image => blob, :name => "WeeklyPoster_#{starttime.to_s}.jpg" )
+    StoredImage.create( :image => blob, :name => "WeeklyPoster.jpg" )
   end
 
 end
