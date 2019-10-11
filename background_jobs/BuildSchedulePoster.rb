6fbb4710e4ce1c6f2cd07a6b70606ac8f9714de2
Maxwell Klein <bklein261@gmail.com>
2019-10-10 23:37:34 -0400

    notifications

--- background_jobs/BuildSchedulePoster.rb
+++ background_jobs/BuildSchedulePoster.rb
@@ -12,20 +12,32 @@ class BuildSchedulePoster
     else
       store = store.update( :image=> File.open(img.path), :saved_on=>DateTime.now )
     end
-    build_quad_poster(img.path)
+    build_quad_poster(img)
     p "finished background job"
   end
 
-  def build_quad_poster(path)
-  	images = [path,path,path,path]
-    processed_image = MiniMagick::Tool::Montage.new do |image|
-      #2550x3300
-      image.geometry "x1225+0+0"
-      image.tile "#{images.size}x1"
-      images.each {|i| image << i}
-      image << "output.jpg"
+  def build_quad_poster(img)
+  	img = img.composite(img.path) do |c|
+      c.gravity "NorthWest"
+      c.geometry '400x400+20+56' 
+  	end
+
+  	store = StoredImage.find( :name => "WeeklyPosterQuad.jpg" )
+    if store.nil? then
+      store = StoredImage.create( :name => "WeeklyPosterQuad.jpg", :image=> File.open(img.path) )
+    else
+      store = store.update( :image=> File.open(img.path), :saved_on=>DateTime.now )
     end
-    p processed_image
+
+  	#images = [path,path,path,path]
+    #processed_image = MiniMagick::Tool::Montage.new do |image|
+    #  #2550x3300
+    #  image.geometry "x1225+0+0"
+    #  image.tile "#{images.size}x1"
+    ##  images.each {|i| image << i}
+    #  image << "output.jpg"
+    #end
+
   end
 
 end
