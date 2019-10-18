c3186d4205d95bf7be8e644d668b586401592f76
Maxwell Klein <bklein261@gmail.com>
2019-10-18 01:04:20 -0400

    eventposter

--- models/Events/EventPoster.rb
+++ models/Events/EventPoster.rb
@@ -37,81 +37,14 @@ module EventPoster
     end
   end
 
-  def SchedulePoster::render_header_line(day, x_offset, y_offset)
-    @@image.combine_options do |i|
-      i.fill "\#FFFFFFFF" unless @@high_contrast
-      i.fill "\#000000FF"     if @@high_contrast
-      i.font "shared/fonts/webfonts/329F99_3_0.ttf"
-      i.pointsize @@header_pointsize
-      i.draw "text #{x_offset},#{y_offset} \"#{SchedulePoster::parse_day(day[:day])}\""
-    end
-  end
-
-  def SchedulePoster::render_text_line(occ, x_offset, y_offset) 
-    col1_x, col2_x, col3_x = 75, 600, 1750
-    col2_trunc, col3_trunc = 53, 28
-
-    @@image.combine_options do |i|
-      i.pointsize @@body_pointsize
-      i.font "shared/fonts/webfonts/329F99_3_0.ttf"
-      case occ[:type]
-      when 'classoccurrence'
-        teachers = occ[:instructors] ? occ[:instructors][0][:name] : ""
-        i.fill "\#FFFFFFFF" unless @@high_contrast
-        i.fill "\#000000FF"     if @@high_contrast
-        i.draw "text #{x_offset + col1_x },#{y_offset} \"#{SchedulePoster::parse_time(occ)}\""
-        i.draw "text #{x_offset + col2_x },#{y_offset} \"#{SchedulePoster::truncate(occ[:title],col2_trunc)}\""
-        i.draw "text #{x_offset + col3_x },#{y_offset} \"w/ #{SchedulePoster::truncate(teachers,col3_trunc)}\""
-      when 'eventsession'
-        i.fill "\#FFFFAAFF" unless @@high_contrast
-        i.fill "\#999900FF"     if @@high_contrast
-        i.draw "text #{x_offset + col1_x },#{y_offset} \"#{SchedulePoster::parse_time(occ)}\""
-        i.draw "text #{x_offset + col2_x },#{y_offset} \"#{SchedulePoster::truncate(occ[:event_title],col2_trunc)}\""
-        i.draw "text #{x_offset + col3_x },#{y_offset} \"#{SchedulePoster::truncate(occ[:title],col3_trunc)}\""
-      when 'private' then
-        i.fill "\#AAAAFFFF" unless @@high_contrast
-        i.fill "\#000099FF"     if @@high_contrast
-        i.draw "text #{x_offset + col1_x },#{y_offset} \"#{SchedulePoster::parse_time(occ)}\""
-        i.draw "text #{x_offset + col2_x },#{y_offset} \"#{SchedulePoster::truncate(occ[:title],col2_trunc)}\""
-      end
-    end
-  end
-
-  def SchedulePoster::build_day(idx, x_offset, y_offset)
-    p "Building Day #{idx} #{Time.now}"  
-    day = @@schedule[idx]
-    box_height = (@@line_height * 1.7) + ( @@line_height * day[:occurrences].count )
-    SchedulePoster::draw_box(x_offset, y_offset, @@box_width, box_height ) unless @@high_contrast
-    x_offset = x_offset + 20
-    y_offset = y_offset + (@@line_height * 1.1)
-    SchedulePoster::render_header_line(day, x_offset, y_offset)
-    y_offset = y_offset + ( @@line_height * 0.1 )
-    day[:occurrences].each do |occ|
-      y_offset = y_offset + @@line_height
-      SchedulePoster::render_text_line(occ, x_offset, y_offset)
-    end
-    y_offset = y_offset + @@line_height*0.5
-    return y_offset
-  end
 
   ################################### RENDERING ######################################
 
   #################################### HELPERS #######################################
 
-  def SchedulePoster::truncate(string,max)
+  def EventPoster::truncate(string,max)
     return "" if string.nil?
     string.length > max ? "#{string[0...max]}..." : string
-  end
-
-  def SchedulePoster::parse_day(str)
-    dt = DateTime.parse(str)
-    dt.strftime("%a %b %e")
-  end 
-
-  def SchedulePoster::parse_time(occ)
-    start   = occ[:starttime]
-    finish  = occ[:endtime]
-    start.strftime("%l:%M %P - ") + finish.strftime("%l:%M %P")
   end
 
   #################################### HELPERS #######################################
