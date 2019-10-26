e5255743dc7a73e16dcc462c44ea0f75b84e3f33
Maxwell Klein <bklein261@gmail.com>
2019-10-26 17:42:34 -0400

    class checkin schedule

--- models/Schedule/Scheduling.rb
+++ models/Schedule/Scheduling.rb
@@ -15,6 +15,14 @@ module Scheduling
     items.group_by { |x| x[:day] }
   end
 
+  def Scheduling::get_checkin_schedule(day)
+    classes = ClassOccurrence.all_between(day, day >> 1).map(&:schedule_details_hash)
+    events  = Scheduling::get_eventsessions_between(day, day >> 1)
+    rentals = Scheduling::get_rentals_between(day, day >> 1)
+    items = events + classes + rentals
+    items.order_by { |x| x[:starttime] }
+  end
+
   def Scheduling::get_classitems_between(from,to)
     occurrences = ClassOccurrence.all_between(from,to).map(&:schedule_details_hash)
     scheduled   = ClassdefSchedule.all.map { |s| s.get_occurrences_with_exceptions_merged(from,to).compact }.flatten
@@ -28,5 +36,7 @@ module Scheduling
   def Scheduling::get_rentals_between(from,to)
     Rental.between(from,to).map(&:schedule_details_hash).compact
   end
+
+
 
 end
