e11ed05bd9808cfc745fe033688fdd5c5c82fac9
Maxwell Klein <bklein261@gmail.com>
2019-12-12 23:12:25 -0500

    event checkout

--- printable/InstagramStory.rb
+++ printable/InstagramStory.rb
@@ -12,7 +12,7 @@ module InstagramStory
     @@lineheight          = 142
   end
  
-  def InstagramStory::generate(event_id, lines)
+  def InstagramStory::generate(event_id, lines, path)
 
     InstagramStory::set_constants
 
@@ -20,7 +20,7 @@ module InstagramStory
     @@image ||= MiniMagick::Image.open "printable/assets/1080x1920_bg.jpg"
 
     @@bubble = MiniMagick::Image.open "printable/assets/#{event_id}"        if event_id.is_a? String
-    @@bubble = MiniMagick::Image.open Event[event_id].image[:original].url if event_id.is_a? Integer
+    @@bubble = MiniMagick::Image.open Event[event_id].image[:original].url  if event_id.is_a? Integer
     @@bubble.to_bubble(lines)
 
     @@image.draw_logo(60,210,960,nil)
@@ -28,50 +28,12 @@ module InstagramStory
     @@image.bubble_shadow(980,980,50,585,5)
     @@image.overlay(@@bubble,980,980,50,585)
 
-    @@image.draw_highlight_text("cosmicfitclub.com/unity",21,0,240,"south","\#CC0000FF")
+    @@image.draw_highlight_text("cosmicfitclub.com#{path}",19,0,240,"south","\#CC0000FF")
 
     @@image.draw_footer(8,170)
 
     @@image
 
   end
-
-  ################################### RENDERING ######################################
-
-  def InstagramStory::draw_lines
-    @@image.combine_options do |i|
-      i.fill "\#FFFFFFFF"
-      i.gravity "south"
-      @@lines.each_with_index do |line,idx|
-        i.font "shared/fonts/webfonts/329F99_3_0.ttf" if idx==0
-        i.pointsize 125                               if idx==0
-        i.font "shared/fonts/webfonts/329F99_B_0.ttf" unless idx==0
-        i.pointsize 110                               unless idx==0
-        i.draw "text 0,#{( @@lines_bottom_margin + @@lineheight * 0.5 + (@@lines.count - idx - 1) * 142 )} \"#{line}\""
-      end
-    end
-  end
-
-  ################################### RENDERING ######################################
-
-  ################################### ELEMENTS #######################################
-
-  def InstagramStory::draw_box(x_offset, y_offset, x, y, x_radius=25, y_radius=25 )
-    @@image.combine_options do |i|
-      i.fill "\#00000099"
-      i.draw "roundRectangle #{x_offset},#{y_offset} #{x_offset + x},#{y_offset + y} #{x_radius},#{y_radius}"
-    end
-  end
-
-  ################################### ELEMENTS #######################################
-
-  #################################### HELPERS #######################################
-
-  def InstagramStory::truncate(string,max)
-    return "" if string.nil?
-    string.length > max ? "#{string[0...max]}..." : string
-  end
-
-  #################################### HELPERS #######################################
 
 end
