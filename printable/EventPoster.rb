af4c7be601830f86a03e1a50272745b63cc95a6a
Maxwell Klein <bklein261@gmail.com>
2020-01-22 00:15:51 -0500

    event checkout

--- printable/EventPoster.rb
+++ printable/EventPoster.rb
@@ -6,12 +6,12 @@ module EventPoster
 
   def EventPoster::generate_all(event_id)
     FileUtils.mkdir_p "printable/results/events/event_#{event_id}"
-    EventPoster::generate4x5(event_id).save("events/event_#{event_id}/event_#{event_id}_4x5.jpg"); p "4x5 complete"
-    EventPoster::generate4x6(event_id).save("events/event_#{event_id}/event_#{event_id}_4x6.jpg"); p "4x6 complete"
+    EventPoster::generate4x5(event_id).save("events/event_#{event_id}/event_#{event_id}_4x5.jpg");             p "4x5 complete"
+    EventPoster::generate4x6(event_id).save("events/event_#{event_id}/event_#{event_id}_4x6.jpg");             p "4x6 complete"
     EventPoster::generate1080x1080(event_id).save("events/event_#{event_id}/event_#{event_id}_1080x1080.jpg"); p "1x1 complete"
     EventPoster::generate1920x1080(event_id).save("events/event_#{event_id}/event_#{event_id}_1920x1080.jpg"); p "HD complete"
     EventPoster::generate1080x1920(event_id).save("events/event_#{event_id}/event_#{event_id}_1080x1920.jpg"); p "Story complete"
-    EventPoster::generateFBEvent(event_id).save("events/event_#{event_id}/event_#{event_id}_fbevent.jpg"); p "FB Event complete"
+    EventPoster::generateFBEvent(event_id).save("events/event_#{event_id}/event_#{event_id}_fbevent.jpg");     p "FB Event complete"
   end
 
   def EventPoster::generate4x5(event_id)
