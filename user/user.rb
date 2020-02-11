782a2b2f52a74a27fb3a3950bc0cfd4a55a28f2f
Maxwell Klein <bklein261@gmail.com>
2020-02-10 21:44:44 -0500

    event edit

--- user/user.rb
+++ user/user.rb
@@ -9,10 +9,11 @@ class CFCuser < Sinatra::Base
 
   set :root, File.dirname(__FILE__)
 
-  get( '/',               :auth => 'user' )     { ref_cust; render_page :user }
-  get( '/wallet_history', :auth => 'user' )     { ref_cust; render_page :wallet_history }
-  get( '/teacher',        :auth => 'teacher' )  { ref_cust; render_page :teacher }
-  get( '/hourly',         :auth => 'frontdesk') { render_page :hourly }
+  get( '/',               :auth => 'user'      ) { ref_cust; render_page :user           }
+  get( '/wallet_history', :auth => 'user'      ) { ref_cust; render_page :wallet_history }
+  get( '/teacher',        :auth => 'teacher'   ) { ref_cust; render_page :teacher        }
+  get( '/hourly',         :auth => 'frontdesk' ) { render_page :hourly                   }
+  get( '/event_collab',   :auth => 'user'      ) { render_page :event_collab             }
   
   error do
     Slack.err( 'User Page Error', env['sinatra.error'] )
