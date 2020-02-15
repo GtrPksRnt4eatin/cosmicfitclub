f8ca1b7a770ed7a115e52e4e5a90a9a7a957cebf
Maxwell Klein <bklein261@gmail.com>
2020-02-15 17:07:00 -0500

    event edit

--- models/Events/Event.rb
+++ models/Events/Event.rb
@@ -157,6 +157,7 @@ class Event < Sequel::Model
       :image_url    => self.thumb_image_url,
       :wide_image   => self.wide_image.try(:details_hash),
       :full_image   => self.image_url,
+      :short_url    => self.short_url,
       :sessions     => self.sessions,
       :prices       => self.available_prices,
       :a_la_carte   => self.a_la_carte
--- models/Setting.rb
+++ models/Setting.rb
@@ -3,7 +3,7 @@ class Setting < Sequel::Model
   
   unrestrict_primary_key
 
-	def to_json
+  def to_json
     val.to_json
   end
 
