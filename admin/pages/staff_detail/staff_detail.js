34809eb2f1972b68b514c362f7d95c76a4c4e9b3
Maxwell Klein <bklein261@gmail.com>
2020-02-23 16:25:02 -0500

    staff hidden

--- admin/pages/staff_detail/staff_detail.js
+++ admin/pages/staff_detail/staff_detail.js
@@ -14,7 +14,7 @@ var ctrl = {
   edit_title:    function(e,m) { edit_text.show("Edit Staff Title", data.staff.title,  function(val) { data.staff.title = val; post_staff_details({ title: val }); } ) },
   edit_bio:      function(e,m) { edit_text.show_long("Edit Staff Bio", data.staff.bio, function(val) { data.staff.bio   = val; post_staff_details({ bio: val   }); } ) },
   edit_customer: function(e,m) { custy_selector.show_modal( data.customer_id, function(val) { post_staff_details({ customer_id: val }); } ) },
-  create_sub:    function(e,m) { $.post("/models/staff/" + data.staff.id + "/create_sub", function() { get_staff_details(); } ); }
+  create_sub:    function(e,m) { $.post("/models/staff/" + data.staff.id + "/create_sub", function() { get_staff_details(); } ); },
   change_hidden: function(e,m) { post_staff_details({ hidden: e.target.value }); }
 }
 
