e64d88fdfeb4655c8460c4ea8df01551617179be
Maxwell Klein <bklein261@gmail.com>
2020-03-07 18:54:45 -0500

    event checkin

--- frontdesk/pages/event_attendance/event_attendance.js
+++ frontdesk/pages/event_attendance/event_attendance.js
@@ -22,7 +22,7 @@ ctrl = {
   },
 
   edit_customer: function(e,m) {
-    document.location.href = '/frontdesk/customer_file?id=' + ( m.tic.purchased_for ? m.tic.purchased_for : m.tic.customer_id );
+    document.location.href = '/frontdesk/customer_file?id=' + m.pass.customer.id;
   },
 
   edit_event(e,m) {
