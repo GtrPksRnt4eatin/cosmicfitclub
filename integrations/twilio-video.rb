d451e6c2a12b81c544d6e3c44ac77e3b631f1da1
Maxwell Klein <bklein261@gmail.com>
2020-04-04 01:23:50 -0400

    cors

--- integrations/twilio-video.rb
+++ integrations/twilio-video.rb
@@ -1,7 +1,7 @@
 class TwilioVideo < Sinatra::Base
 
   post '/' do
-  	Slack.webhook("Video Webhook", JSON.pretty_generate params)
+  	Slack.webhook("Video Webhook", JSON.pretty_generate(params))
   	status 204
   	""
   end
