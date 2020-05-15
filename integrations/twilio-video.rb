14863ee823250fecf9f2e3ebb128a4f462e917ca
Maxwell Klein <bklein261@gmail.com>
2020-05-15 15:38:58 -0400

    twilio webhooks

--- integrations/twilio-video.rb
+++ integrations/twilio-video.rb
@@ -1,7 +1,13 @@
 class TwilioVideo < Sinatra::Base
 
-  post '/' do
-  	Slack.webhook("Video Webhook", params.to_json);
+  post '/' 
+    Slack.webhook("Video Webhook", params[:StatusCallbackEvent]);
+  	case params[:StatusCallbackEvent]
+  	when 'room-created'
+  	when 'room-ended'
+  	when 'participant-connected'
+  	when 'participant-disconnected'
+  	end
   	status 204
   	""
   end
