80b2d51f16fe3463b1770cb815ea9d23663c2bcd
Maxwell Klein <bklein261@gmail.com>
2020-05-15 16:46:48 -0400

    twilio webhooks

--- integrations/twilio-video.rb
+++ integrations/twilio-video.rb
@@ -9,7 +9,7 @@ class TwilioVideo < Sinatra::Base
   	when 'room-ended'
   		client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_AUTH_TOKEN']
   		list = client.video.rooms(params[:RoomSid]).participants.list()
-  		list = list.map{ |x| "#{x[:identity] - x[:start_time] - x[:duration]}"}.join("\r\n")
+  		list = list.map{ |x| "#{x.identity} - #{x.start_time} - #{x.duration}"}.join("\r\n")
   		Slack.webhook("Video Room Ended:", "```#{list}```")
   	when 'participant-connected'
   	when 'participant-disconnected'
