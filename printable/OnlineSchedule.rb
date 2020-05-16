e39e88df19692474260da1075a2dd64f2ad1a8a3
Maxwell Klein <bklein261@gmail.com>
2020-05-16 12:51:09 -0400

    fix thumb_url bug

---
+++ printable/OnlineSchedule.rb
@@ -0,0 +1,16 @@
+
+module OnlineSchedule
+
+	def OnlineSchedule::generate_all
+
+	end
+
+	def OnlineSchedule::generate_instagram_story
+
+	end
+
+	def OnlineSchedule::generate_fb_post
+
+	end
+
+end
