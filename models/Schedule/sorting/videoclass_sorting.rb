3b45a342317eb6b288fdc87ff21650e89c4d6b0f
Maxwell Klein <bklein261@gmail.com>
2020-06-02 13:58:18 -0400

    scheduling

--- models/Schedule/sorting/videoclass_sorting.rb
+++ models/Schedule/sorting/videoclass_sorting.rb
@@ -11,7 +11,7 @@ module Scheduling
     items   = events + classes + rentals
     items   = items.sort_by { |x| x[:starttime] }
     { :earlier_today   => items.select { |x| (Date.today.to_time..(Time.now+(5*60))).cover? x[:endtime] },
-      :happening_now   => items.select { |x| ((x[:starttime]-(5*60)..(x[:endtime]-(5*60))).cover? Time.now },
+      :happening_now   => items.select { |x| ((x[:starttime]-(5*60))..(x[:endtime]-(5*60))).cover? Time.now },
       :later_today     => items.select { |x| ((Time.now+(5*60))..(Date.today+1).to_time).cover? x[:starttime] },
       :later_this_week => items.select { |x| (Date.today+1).to_time < x[:starttime] }.group_by { |x| x[:day] },
       :flat_list       => items,
