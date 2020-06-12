8da9132f21f9e6d4f0063e05f980addca5ccaaf7
Maxwell Klein <bklein261@gmail.com>
2020-06-12 13:09:24 -0400

    image uploader

--- printable/schedulePromo.rb
+++ printable/schedulePromo.rb
@@ -3,10 +3,53 @@ module SchedulePromo
     grouped = ClassdefSchedule.all.group_by { |x| { :class=> x.classdef.name, :teacher=>x.teachers.map(&:name).join(', ') } }
     list = grouped.map { |k,v| { :teacher => k[:teacher], :img => v[0].image_url, :lines => [k[:class], "w/ " + k[:teacher], v.map(&:simple_meeting_time_description).join(", ")] } } 
     list.each{ |x| SchedulePromo::generate4x5(x).save("vidpromos/schedules/#{x[:lines][0]} - #{x[:teacher]}.jpg") }
+    list.each{ |x| SchedulePromo::generate_fbevent(x).save("vidpromos/fbevent/#{x[:lines][0]} - #{x[:teacher]}.jpg") }
+  end
+
+  def SchedulePromo::generateall_fbevent()
+    grouped = ClassdefSchedule.all.group_by { |x| { :class=> x.classdef.name, :teacher=>x.teachers.map(&:name).join(', ') } }
+    list = grouped.map { |k,v| { :teacher => k[:teacher], :img => v[0].image_url, :lines => [k[:class], "w/ " + k[:teacher], v.map(&:simple_meeting_time_description).join(", ")] } } 
+    list.each{ |x| SchedulePromo::generate_fbevent(x).save("vidpromos/fbevent/#{x[:lines][0]} - #{x[:teacher]}.jpg") }
   end
 
   def SchedulePromo::generate4x5(x)
     image = MiniMagick::Image.open("printable/assets/4x5_bg.jpg")
+    image.draw_elements([
+      { :type     => 'logo',
+        :x_offset => 640,
+        :y_offset => 50,
+        :width    => 400
+      },
+      { :type     => "highlight_text",
+        :x_offset => 0,
+        :y_offset => 175,
+        :ptsize   => 18.5,
+        :gravity  => "Northeast",
+        :text     => "video.cosmicfitclub.com"
+      },
+      { :type     => 'image_bubble',
+        :x_offset => 50,
+        :y_offset => 50,
+        :width    => 905,
+        :height   => 905,
+        :margin   => 5,
+        :ptscale  => 0.05,
+        :ptscale2 => 0.9,
+        :img      => x[:img],
+        :lines    => x[:lines]
+      },
+      { :type     => "highlight_text",
+        :x_offset => 0,
+        :y_offset => 20,
+        :ptsize   => 12.5,
+        :gravity  => "South",
+        :text     => "Live Video Fitness Classes Everyday!"
+      }
+    ])
+  end
+
+  def SchedulePromo::generate_fbevent(x)
+    image = MiniMagick::Image.open("printable/assets/fb_event_bg.jpg")
     image.draw_elements([
       { :type     => 'logo',
         :x_offset => 340,
@@ -40,11 +83,5 @@ module SchedulePromo
       }
     ])
   end
+
 end
-
-
-
-
-
-
-[1,4,7,9,13]
