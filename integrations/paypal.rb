25e873c14634fd5330035e59dd9137bd72a7e156
Maxwell Klein <bklein261@gmail.com>
2020-07-11 02:07:48 -0400

    paypal api

--- integrations/paypal.rb
+++ integrations/paypal.rb
@@ -1,3 +1,13 @@
+require 'paypal-sdk-rest'
+include PayPal::SDK::REST
+
+PayPal::SDK::REST.set_config(
+  :mode => 'live',
+  :client_id => ENV['PAYPAL_ID'],
+  :client_secret => ENV['PAYPAL_SECRET'],
+  :ssl_options => {}
+)
+
 class PayPalRoutes < Sinatra::Base
 
   post '/webhooks' do 
