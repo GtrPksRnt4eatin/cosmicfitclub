e1b3ff44f7ee60740cdd40571fdd6b3adc5b4ba5
Maxwell Klein <bklein261@gmail.com>
2020-07-20 21:49:23 -0400

    paypal payouts

--- integrations/paypal.rb
+++ integrations/paypal.rb
@@ -6,15 +6,48 @@ module PayPalSDK
   define_singleton_method(:client) do
     @@client
   end
-end
-#include PayPal::SDK::REST
 
-#PayPal::SDK::REST.set_config(
-#  :mode => 'live',
-#  :client_id => ENV['PAYPAL_ID'],
-#  :client_secret => ENV['PAYPAL_SECRET'],
-#  :ssl_options => {}
-#)
+  def payout_batch_header 
+    { sender_batch_header:{
+      recipient_type: 'EMAIL',
+      email_message: 'SDK Payouts Test',
+      note: 'testing testing testing',
+      sender_batch_id: rand(36**8).to_s(36),
+      email_subject: 'Test of Paypal Payout System'
+    }
+  end
+
+  def payout_batch_item(arg)
+    { receiver: arg[:recipient],
+      amount: {
+        currency: 'USD',
+        value: '%.2f' % arg[:amount]
+      },
+      note: arg[:note],
+      sender_item_id: rand(36**8).to_s(36)
+    }
+  end
+
+  def PayPalSDK::payout_batch(items)
+    body = {
+      sender_batch_header: payout_batch_header,
+      items: [
+        items.map { |x| payout_batch_item(x) }
+      ]
+    }
+    request = PaypalPayoutsSdk::Payouts::PayoutsPostRequest::new
+    request.request_body(body) 
+    begin
+      response = client.execute(request)
+      batch_id = response.result.batch_header.payout_batch_id
+      return batch_id
+    rescue PayPalHttp::HttpError => ioe
+      puts ioe.status_code
+      puts ioe.headers["debug_id"]
+    end
+  end
+  puts
+end
 
 class PayPalRoutes < Sinatra::Base
 
