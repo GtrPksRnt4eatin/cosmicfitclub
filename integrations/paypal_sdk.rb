8cc70e8ae2db2689babe9b60d338109176b28ab0
Maxwell Klein <bklein261@gmail.com>
2020-07-21 15:44:03 -0400

    email photos

---
+++ integrations/paypal_sdk.rb
@@ -0,0 +1,47 @@
+require 'paypal-payouts-sdk'
+
+module PayPalSDK
+  @@environment = PayPal::LiveEnvironment.new(ENV['PAYPAL_ID'],ENV['PAYPAL_SECRET'])
+  @@client = PayPal::PayPalHttpClient.new(@@environment) 
+  define_singleton_method(:client) do
+    @@client
+  end
+
+  def PayPalSDK::payout_batch_header 
+    { recipient_type: 'EMAIL',
+      email_message: 'SDK Payouts Test',
+      note: 'testing testing testing',
+      sender_batch_id: rand(36**8).to_s(36),
+      email_subject: 'Test of Paypal Payout System'
+    }
+  end
+
+  def PayPalSDK::payout_batch_item(arg)
+    { receiver: arg[:recipient],
+      amount: {
+        currency: 'USD',
+        value: '%.2f' % arg[:amount]
+      },
+      note: arg[:note],
+      sender_item_id: rand(36**8).to_s(36)
+    }
+  end
+
+  def PayPalSDK::payout_batch(items)
+    body = {
+      sender_batch_header: payout_batch_header,
+      items: items.map { |x| payout_batch_item(x) }
+    }
+    request = PaypalPayoutsSdk::Payouts::PayoutsPostRequest::new
+    request.request_body(body) 
+    begin
+      response = client.execute(request)
+      batch_id = response.result.batch_header.payout_batch_id
+      return batch_id
+    rescue PayPalHttp::HttpError => ioe
+      p ioe.result.message
+      puts ioe.status_code
+    end
+  end
+  puts
+end
--- utillities/db_models.rb
+++ utillities/db_models.rb
@@ -5,19 +5,18 @@ require_relative '../ruby/patches'
 require_relative '../integrations/database'
 require_relative '../integrations/aws'
 require_relative '../integrations/sheets'
+require_relative '../integrations/paypal_sdk'
 require_relative '../integrations/stripe_methods'
 require_relative '../ruby/shrine'
 
-Dir["../models/mixins/*.rb"].each  { |file| require_relative file unless /.*Routes.*/=~file }
-Dir["../models/**/*.rb"].each      { |file| require_relative file unless /.*Routes.*/=~file }
+Dir["../models/mixins/*.rb"].each { |file| require_relative file unless /.*Routes.*/=~file }
+Dir["../models/**/*.rb"].each     { |file| require_relative file unless /.*Routes.*/=~file }
 Dir["../printable/lib/*.rb"].each { |file| require_relative file }
 Dir["../printable/*.rb"].each     { |file| require_relative file }
 
-
-
 def reload
-  Dir["printable/lib/*.rb"].each { |file| load file }
-  Dir["printable/*.rb"].each        { |file| load file }
+  Dir["printable/lib/*.rb"].each  { |file| load file }
+  Dir["printable/*.rb"].each      { |file| load file }
 end
 
 Dir.chdir("..")
