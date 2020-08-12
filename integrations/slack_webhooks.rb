54d0dd37d0695e04526ce2560b06d2955279d085
Maxwell Klein <bklein261@gmail.com>
2020-08-12 18:01:41 -0400

    slackbot

--- integrations/slack_webhooks.rb
+++ integrations/slack_webhooks.rb
@@ -1,8 +1,32 @@
+require 'sucker_punch'
+require 'slack-ruby-client'
+
+Slack.configure do |config|
+  config.token = ENV['SLACK_TOKEN']
+end
 
 class SlackBot < Sinatra::Base
 
   post '/dailyPromo' do
-    p params
+    date = Date.parse(params["text"]) || Date.today
+    PostDailyPromo.perform(date)
+    "Generating Promo... Please Wait!"
   end
 
+end
+
+class PostDailyPromo
+  include SuckerPunch::Job
+  def perform(date)
+    promo = DailyPromo::generate_all(date)
+    client = Slack::Web::Client.new
+    client.files_upload(
+      channels: '#promotional_materials',
+      as_user: false,
+      file: Faraday::UploadIO.new(promo.path, "image/jpeg"),
+      title: "#{date.to_s} Promo",
+      filetype: 'jpg',
+      filename: "#{date.to_s}_promo.jpg"
+    )
+  end
 end
