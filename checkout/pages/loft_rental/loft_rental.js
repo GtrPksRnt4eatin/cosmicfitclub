6dcecf3b6a5166d2721a27e31df6735d50539eb3
Maxwell Klein <bklein261@gmail.com>
2021-03-14 17:36:15 -0400

    loft_rental page

---
+++ checkout/pages/loft_rental/loft_rental.js
@@ -0,0 +1,12 @@
+data = {
+  rental: {
+    starttime: ''
+  }
+};
+
+$(document).ready( function() {
+
+    include_rivets_dates();
+    var binding = rivets.bind( $('body'), { data: data } );arguments
+
+});
