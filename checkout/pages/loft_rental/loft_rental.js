12fef2c44f12263888bc495188a268fbe90cdc61
Maxwell Klein <bklein261@gmail.com>
2021-04-12 22:13:42 -0400

    loft rental form

--- checkout/pages/loft_rental/loft_rental.js
+++ checkout/pages/loft_rental/loft_rental.js
@@ -3,13 +3,27 @@ data = {
     starttime: '',
     endtime: '',
     activity: '',
-    note: ''
+    note: '',
+    num_slots: 0
   }
 };
 
+ctrl = {
+  set_num_slots: function(e,m) {
+    data.num_slots = itoa(e.target.value);
+  },
+
+  choose_custy: function(e,m) {
+    custy_selector.choose_custy();
+  }
+
+}
+
 $(document).ready( function() {
 
-    include_rivets_dates();
-    var binding = rivets.bind( $('body'), { data: data } );arguments
+  include_rivets_dates();
+  var binding = rivets.bind( $('body'), { data: data, ctrl: ctrl } );
+
+  custy_selector = new CustySelector();
 
 });
