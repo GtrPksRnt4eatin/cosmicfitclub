4bf1a5398b25b2e9dc00adc59fc08a1f83d79803
Maxwell Klein <ben@cosmicfitclub.com>
2021-07-13 14:51:04 -0400

    event checkout privates

--- checkout/pages/event/event.js
+++ checkout/pages/event/event.js
@@ -53,7 +53,7 @@ function initialize_rivets() {
   rivets.formatters.equals    = function(val,val2)    { return val== val2;                            }
   rivets.formatters.is_member = function(val)         { return empty(val) ? false : !empty(val.plan); } 
   rivets.formatters.num_tix   = function(val)         { return ( val && val > 1 ) ? val + ' Tickets ' : ''; }
-  rivets.formatters.first_price_title = function(val) { return val[0] ? val[0].title : ""; }
+  rivets.formatters.first_price_title = function(val) { return ( val && val[0] ) ? val[0].title : ""; }
   rivets.formatters.diff_days = function(val,val2)    { return !moment(val).isSame(moment(val2), 'date'); }
 
   rivets.bind( $('body'), { customer: CUSTOMER, data: data, ctrl: ctrl } );  
