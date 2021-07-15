d298ce1dc506332c008bcdc49a450aa36ddeba29
Maxwell Klein <ben@cosmicfitclub.com>
2021-07-14 23:42:32 -0400

    event payments

--- models/GroupReservations/GroupReservation.rb
+++ models/GroupReservations/GroupReservation.rb
@@ -3,4 +3,9 @@ class GroupReservation < Sequel::Model
     many_to_one :customer
 
     one_to_many :slots, :class => :GroupReservationSlot
+
+    def before_create
+      self.tag = rand(36**8).to_s(36)
+    end
+
 end
