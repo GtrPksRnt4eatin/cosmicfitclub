feecb1f61a1be259fd05eb10e3fd30c2272c1a36
Maxwell Klein <ben@cosmicfitclub.com>
2021-09-13 12:26:25 -0400

    password reset

--- auth/auth.rb
+++ auth/auth.rb
@@ -4,6 +4,7 @@ require 'sinatra/base'
 require 'sinatra/cross_origin'
 require 'sinatra/cookies'
 require_relative './omniauth'
+require_relative './auth_helpers'
 require_relative './models/User'
 require_relative './models/Role'
 require_relative './models/Omniaccount'
@@ -240,79 +241,6 @@ class CFCAuth < Sinatra::Base
   error do
     Slack.err( 'Auth Error', env['sinatra.error'] )
     ENV['sinatra.error']
-  end
-
-end
-
-module Sinatra
-
-  module Auth
-  
-    module Helpers
-      def logged_in?       ; !!session[:user_id]             end
-      def user             ; User[session[:user_id]]         end
-      def customer         ; Customer[session[:customer_id]] end
-      
-      def has_role?(role)
-        User[session[:user_id]].has_role? role
-      end
-
-      def read_jwt
-        jwt = request.cookies['cosmicjwt'] or return
-        jwt = JWT.decode(jwt,ENV['JWT_SECRET'],true,{ algorithm: 'HS256'})
-        custy = Customer[jwt[0]["user"]["customer_id"]] or return
-        session[:customer_id] = custy.id
-        session[:user_id] = custy.user.id
-        jwt
-      end
-
-    end
-  
-    def self.registered(app)
-
-      app.helpers Auth::Helpers
-
-      app.set(:auth) do |role|
-        condition do
-          redirect "/auth/login?page=#{request.path}" unless logged_in?
-          redirect '/' unless User[session[:user_id]].has_role? role
-          true
-        end
-      end
-
-      app.set(:onboard) do |role|
-        condition do
-          redirect "/auth/onboard?page=#{request.path}" unless logged_in?
-          redirect '/' unless User[session[:user_id]].has_role? role
-          true
-        end
-      end
-
-      app.set(:self_or) do |role|
-        condition do
-          return true if User[session[:user_id]].has_role? role
-          halt(401, "Cannot Modify Someone Elses Account!") unless session[:customer_id] == Integer(params[:customer_id])
-          true
-        end
-      end
-
-      app.set(:jwt_logged_in) do |bool|
-        condition do
-          read_jwt
-          true
-        rescue JWT::DecodeError
-          halt(401, 'A token must be passed.')
-        rescue JWT::ExpiredSignature
-          halt(403, 'The token has expired.')
-        rescue JWT::InvalidIssuerError
-          halt(403, 'The token does not have a valid issuer.')
-        rescue JWT::InvalidIatError
-          halt(403, 'The token does not have a valid "issued at" time.')
-        end
-      end 
-
-    end
-
   end
 
 end
