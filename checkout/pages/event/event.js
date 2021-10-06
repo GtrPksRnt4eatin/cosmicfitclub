47794739495ebbf55b66a5f8067e8b34ccf51008
Maxwell Klein <ben@cosmicfitclub.com>
2021-10-05 23:54:51 -0400

    niko checkout

--- checkout/pages/event/event.js
+++ checkout/pages/event/event.js
@@ -347,10 +347,7 @@ var ctrl = {
 function checkout_new() {
   calculate_total();
 
-  let desc = data.event_data.name + ': ';
-  desc += rivets.formatters.fulldate(data.selected_timeslot.starttime); 
-  desc += ' - ';
-  desc  += rivets.formatters.time(data.selected_timeslot.endtime); 
+  let desc = data.event_data.name;
 
   pay_form.checkout(userview.id, data.total_price, desc ,null, function(payment_id) {
 
