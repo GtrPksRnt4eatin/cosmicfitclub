cd9fc781751bd8548bed446d5d6855f059e2f625
Maxwell Klein <ben@cosmicfitclub.com>
2022-01-07 16:07:42 -0500

    giftcards

--- offers/offers.rb
+++ offers/offers.rb
@@ -16,6 +16,7 @@ class CFCOffers < Sinatra::Base
   get( '/newstudent'  )  { render_page :new_student  }
   get( '/redeem_gift' )  { render_page :gift_cert    }
   get( '/survey'      )  { render_page :survey       }
+  get( '/giftcard'    )  { render_page :holiday_sale }
 
   not_found do
     @err="Sorry, This Offer Is No Longer Available."
