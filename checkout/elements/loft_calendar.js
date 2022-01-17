7469e3dda68fbb99454a5ad03f85a6c6cd72804f
Maxwell Klein <ben@cosmicfitclub.com>
2022-01-16 22:38:48 -0500

    loft registration

---
+++ checkout/elements/loft_calendar.js
@@ -0,0 +1,58 @@
+function LoftCalendar(parent) {
+
+	this.state = {
+      window_start: null,
+      window_end: null,
+      daypilot: null,
+      reservations: null   
+	}
+
+	this.bind_handlers(['build_daypilot']);
+	this.build_dom(parent);
+	this.load_styles();
+	this.bind_dom();
+
+}
+
+LoftCalendar.prototype = {
+  constructor: LoftCalendar,
+
+  build_daypilot: function() {
+    this.state.daypilot = new DayPilot.Calendar('daypilot', {
+      viewType: "Week",
+      cellDuration: 30,
+      cellHeight: 25,
+      businessBeginsHour: 12,
+      businessEndsHour: 23,
+      dayBeginsHour: 12,
+      dayEndsHour: 23,
+      timeRangeSelectedHandling: "Enabled",
+      onTimeRangeSelected: this.on_timeslot_selected,
+      eventDeleteHandling: "Disabled",
+      eventMoveHandling: "Disabled",
+      eventResizeHandling: "Disabled",
+      eventClickHandling: "Disabled",
+      eventHoverHandling: "Disabled",
+    });
+  },
+
+  on_timeslot_seleted: function(args) {
+      
+  }
+
+}
+
+Object.assign( LoftCalendar.prototype, element);
+Object.assign( LoftCalendar.prototype, ev_channel);
+
+LoftCalendar.prototype.HTML = `
+  <div class='loftcalendar'></div>
+`.untab(2);
+
+LoftCalendar.prototype.CSS = `
+  
+  .loftcalendar {
+
+  }
+
+`.untab(2);
--- checkout/pages/loft_rental/loft_rental.js
+++ checkout/pages/loft_rental/loft_rental.js
@@ -42,59 +42,23 @@ $(document).ready( function() {
 
   userview       = new UserView(id('userview_container'));
   popupmenu      = new PopupMenu( id('popupmenu_container') );
+  loft_calendar  = new LoftCalendar(id('loft_calendar_container'));
   custy_selector = new CustySelector();
 
   custy_selector.ev_sub('show'       , popupmenu.show );
   custy_selector.ev_sub('close_modal', popupmenu.hide );
   
-  setup_daypilot();
+  loft_calendar.build_daypilot();
 });
 
-function setup_daypilot() {
-  daypilot = new DayPilot.Calendar('daypilot', {
-    viewType: "Week",
-    cellDuration: 60,
-    cellHeight: 50,
-    businessBeginsHour: 15,
-    businessEndsHour: 22,
-    dayBeginsHour: 15,
-    dayEndsHour: 22,
-    timeRangeSelectedHandling: "Enabled",
-    onTimeRangeSelected: async (args) => {
-      const modal = await DayPilot.Modal.prompt("Create a new event:", "Event 1");
-      const dp = args.control;
-      dp.clearSelection();
-      if (modal.canceled) { return; }
-      dp.events.add({
-        start: args.start,
-        end: args.end,
-        id: DayPilot.guid(),
-        text: modal.result
-      });
-    },
-    eventDeleteHandling: "Disabled",
-    eventMoveHandling: "Update",
-    onEventMoved: (args) => {
-      args.control.message("Event moved: " + args.e.text());
-    },
-    eventResizeHandling: "Update",
-    onEventResized: (args) => {
-      args.control.message("Event resized: " + args.e.text());
-    },
-    eventClickHandling: "Disabled",
-    eventHoverHandling: "Disabled",
-  });
-
-  /*
-  daypilot.viewType = "Days";
-  daypilot.days = 5;
-  daypilot.startDate = "2021-08-09"
-  daypilot.businessBeginsHour = 15;
-  daypilot.businessEndsHour = 22;
-  daypilot.dayBeginsHour = 11;
-  daypilot.dayEndsHour = 22;
-  daypilot.heightSpec = "BusinessHoursNoScroll"
-  */
-  daypilot.init();
+function on_timeslot_selected(args) {
+  if(!userview.logged_in) { userview.onboard(); return;  }
+  data.selected_timeslot.starttime = new Date(args.start.value);
+  data.selected_timeslot.endtime = new Date(data.selected_timeslot.starttime.getTime() + 60 * 60 * 1000)
+  data.num_slots = 2;
+  data.rental.slots = [];
+  data.rental.slots.push( { customer_id: userview.id, customer_string: userview.custy_string } );
+  data.rental.slots.push( { customer_id: 0, customer_string: "Add Student" } );
+  calculate_total();
 }
 
