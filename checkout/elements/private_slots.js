db6d23b42e1db016634e389a67bc7f8a46c7c195
Maxwell Klein <ben@cosmicfitclub.com>
2022-05-31 00:58:09 -0400

    private slots checkout

--- checkout/elements/private_slots.js
+++ checkout/elements/private_slots.js
@@ -5,12 +5,16 @@ function PrivateSlots(parent,attr) {
 	this.state = {
       sessions: [],
       attendance: [],
+      selected_session: null,
+      included_sessions: [],
+      num_slots: 1,
+      rental: null,
       starttime: null,
       endtime: null,
       page: 0
 	}
 
-	this.bind_handlers(['build_daypilot','load_sessions','get_attendance','update_daypilot_colors','on_session_selected']);
+	this.bind_handlers(['build_daypilot','load_sessions','get_attendance','update_daypilot_colors','session_available','on_session_selected','sort_included_sessions','set_included_sessions','toggle_included_sessions','clear_session','set_num_slots']);
 	this.load_styles();
 	//this.bind_dom();
 
@@ -79,23 +83,80 @@ PrivateSlots.prototype = {
 
         let full     = attendance.passes.length >= session.max_capacity || ( session.title == "Private" && attendance.passes.length > 0 );
         let selected = data.included_sessions.includes(x.id());
-        
+
         x.client.backColor( full ? "#AAAAAA" : selected ? "#CCCCFF" : "#FFFFFF" );
 
         this.daypilot.events.update(x);
       }.bind(this));
     },
 
+
+    session_available(id) {
+      let session    = this.event.sessions.find(   function(y) { return id == y.id; } );
+      let attendance = this.state.attendance.find( function(z) { return id == z.id; } );
+      if( !attendance || !session ) return false;
+      if( attendance.passes.length >= session.max_capacity || ( session.title == "Private" && attendance.passes.length > 0 ) ) return false;
+      return true;
+    },
+
     on_session_selected: function(args) {
         if(args.originalEvent.type=='touchend') { return; }
         if(!userview.logged_in) { userview.onboard(); return;  }
-        if( !session_available(args.e.data.id) ) { return; }
-      
-        clear_selected_price();
-        toggle_included_session(args.e.data);
-        calculate_custom_prices();
-        calculate_total();
+        if( !this.session_available(args.e.data.id) ) { return; }
+
+        this.state.selected_session = this.event.sessions.find( function(x) { return args.e.data.id == x.id; });
+        this.toggle_included_session(args.e.data);
         this.update_daypilot_colors();
+        ev_fire('on_session_selected', this.state.selected_session);
+      
+        //clear_selected_price();
+        //calculate_custom_prices();
+        //calculate_total();
+    },
+
+    sort_included_sessions() {
+      this.state.included_sessions.sort( function(a,b) { 
+        let sess_a = this.event.sessions.find( function(x) { return x.id == a; } );
+        let sess_b = this.event.sessions.find( function(x) { return x.id == b; } );
+        if(!sess_a) { return sess_b ? 1 : 0; }
+        if(!sess_b) { return sess_a ? -1 : 0; }
+        return moment(sess_a.start_time) - moment(sess_b.start_time);
+      });
+    },
+    
+    set_included_sessions(sessions) {
+      for(var i=0; i < this.event.sessions.length; i++) { 
+        this.event.sessions[i].selected = sessions.indexOf(this.event.sessions[i].id)!=-1;
+      }
+      this.state.included_sessions = sessions.slice(0);
+      sort_included_sessions();
+    },
+    
+    toggle_included_session(session) {
+      sessions = this.state.included_sessions;
+      var i = sessions.indexOf(session.id);
+      if(i==-1) { sessions.push(session.id); }
+      else { 
+        if(sessions.length == 1) { sessions = []; }
+        else { sessions.splice(i, 1); }
+      }
+      set_included_sessions(sessions);
+    },
+
+    clear_session() {
+      this.state.selected_session = null;
+    },
+
+
+    set_num_slots(e,m) {
+      this.state.num_slots = parseInt(e.target.value);
+      this.state.num_slots = isNaN(this.state.num_slots) ? 0 : this.state.num_slots;
+      while(this.state.rental.slots.length<this.state.num_slots) {
+       this.state.rental.slots.push({ customer_id: 0, customer_string: 'Add Student' }); 
+      }
+      while(this.state.rental.slots.length>this.state.num_slots){
+        this.state.rental.slots.pop();
+      }
     }
 }
 
@@ -104,7 +165,7 @@ Object.assign( PrivateSlots.prototype, ev_channel);
 
 PrivateSlots.prototype.HTML = ES5Template(function(){/**
   <div id='private_slots'>
-    <div rv-show='state.page | equals 0'>  
+    <div rv-hide='state.selected_session'>  
       <hr class='mobile'>
       <br class='mobile'>
       
@@ -112,6 +173,40 @@ PrivateSlots.prototype.HTML = ES5Template(function(){/**
       <br>
 
       <div id='daypilot'></div>
+    </div>
+
+    <div class='selected_timeslot' rv-show='state.selected_session'>
+      <h2>{ data.selected_timeslot.starttime | fulldate } - {data.selected_timeslot.endtime | time }</h2>
+      <span style="cursor:pointer; color: #9999FF;" rv-on-click="clear_session">change session</span>
+      <br/><br/>
+
+      <div class='tuple'>
+        <div class='attrib'># People</div>
+        <div class='value'>
+          <select class='num_students' rv-on-change='set_num_slots'>
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select>  
+        </div>
+      </div> 
+
+      <div rv-if='data.num_slots'>
+        <hr/>
+        <div class='tuple' rv-each-slot='data.rental.slots'>
+          <div class='attrib'>
+            Slot #{index | fix_index}
+          <div class='value edit' rv-on-click='ctrl.choose_custy'>
+            {slot.customer_string}           
+          </div>
+          <hr/>
+
+          <div>
+            <button id='checkout' rv-on-click='ctrl.checkout_new'>
+              Pay { data.total_price | money } Now
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
 
   </div>
