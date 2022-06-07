e0aac9a7870d5c83ea7f740301b8db6ec5280cf0
Maxwell Klein <ben@cosmicfitclub.com>
2022-06-07 02:30:24 -0400

    event checkout

--- checkout/elements/session_chooser.js
+++ checkout/elements/session_chooser.js
@@ -101,7 +101,7 @@ function SessionChooser(parent,attr) {
           this.session = this.event.sessions.find( function(x) { return args.e.data.id == x.id; });
           //this.toggle_included_session(args.e.data);
           //this.update_daypilot_colors();
-          //this.ev_fire('on_session_selected', this.session);
+          this.ev_fire('on_session_selected', this.session);
       },
   
       sort_included_sessions() {
