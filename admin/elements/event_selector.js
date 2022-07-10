409ac43dceee3be5c2f7e25c3268cfc8639674d5
Maxwell Klein <ben@cosmicfitclub.com>
2022-07-10 15:00:54 -0400

    transfer event tickets

--- admin/elements/event_selector.js
+++ admin/elements/event_selector.js
@@ -29,9 +29,9 @@ Object.assign( EventSelector.prototype, ev_channel);
   
 EventSelector.prototype.HTML = ES5Template(function(){/**
   <div class='EventSelector form'>
-    <select rv-selectize='state.event_id''>
+    <select rv-selectize='state.event_id'>
       <option value='0'>None</option>
-      <option rv-each-event='state.events' rv-value='state.event_id'>
+      <option rv-each-event='state.events' rv-value='event.id'>
         { event.name }
       </option>
   </div>
