ab68aec5ddb6474a64630a76df9a779d19e03a7c
Maxwell Klein <ben@cosmicfitclub.com>
2022-07-10 13:26:50 -0400

    transfer event tickets

--- admin/elements/event_selector.js
+++ admin/elements/event_selector.js
@@ -1,43 +1,39 @@
 function EventSelector(el,attr) {
-
-    this.state = {
-      events: attr['events'],
-      event_id: 0
-    }
-  
-    this.bind_handlers(['fetch_events']);
-    this.load_styles();
-    this.state.events || this.fetch_events();
+  this.state = {
+    events: attr['events'],
+    event_id: 0
   }
   
-  EventSelector.prototype = {
-    constructor: EventSelector,
-
-    fetch_events() {
-      $.get('/models/events/list', function(val) { this.state.events = val; }.bind(this) );
-    }
-    
-  }
-  
-  Object.assign( EventSelector.prototype, element);
-  Object.assign( EventSelector.prototype, ev_channel); 
-  
-  EventSelector.prototype.HTML = ES5Template(function(){/**
-    <div class='EventSelector form'>
-      <select rv-selectize='state.event_id'>
-        <option value='0'>None</option>
-        <option rv-each-event='state.events' rv-value='event.id'>
-          { event.starttime | dateformat 'ddd MMM Do YYYY'} [{event.id}] { event.name }
-        </option>
-    </div>
-  **/}).untab(2);
-  
-  EventSelector.prototype.CSS = ES5Template(function(){/**
-    .EventSelector { }
-  **/}).untab(2);
-  
+  this.bind_handlers(['fetch_events']);
+  this.load_styles();
+  this.state.events || this.fetch_events();
 }
+  
+EventSelector.prototype = {
+  constructor: EventSelector,
 
+  fetch_events() {
+    $.get('/models/events/list', function(val) { this.state.events = val; }.bind(this) );
+  }
+}
+  
+Object.assign( EventSelector.prototype, element);
+Object.assign( EventSelector.prototype, ev_channel); 
+  
+EventSelector.prototype.HTML = ES5Template(function(){/**
+  <div class='EventSelector form'>
+    <select rv-selectize='state.event_id'>
+      <option value='0'>None</option>
+      <option rv-each-event='state.events' rv-value='event.id'>
+        { event.starttime | dateformat 'ddd MMM Do YYYY'} [{event.id}] { event.name }
+      </option>
+  </div>
+**/}).untab(2);
+  
+EventSelector.prototype.CSS = ES5Template(function(){/**
+  .EventSelector { }
+**/}).untab(2);
+  
 rivets.components['event-selector'] = { 
   template:   function()        { return EventSelector.prototype.HTML; },
   initialize: function(el,attr) { return new EventSelector(el,attr);   }
