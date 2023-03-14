4e49d41ba6e3c861fec6297701e90e317286fe7c
Benjamin Maxwell <bklein261@gmail.com>
2023-03-14 11:11:43 -0400

    Update loft_rental.js

--- checkout/pages/loft_rental/loft_rental.js
+++ checkout/pages/loft_rental/loft_rental.js
@@ -50,10 +50,15 @@ $(document).ready( function() {
   custy_selector.ev_sub('close_modal', popupmenu.hide );
 
   loft_calendar.ev_sub('on_timeslot_selected', function(val) {
-
+    if(!userview.logged_in) { userview.onboard(); return;  }
     console.log(val);
-    data.selected_timeslot = val;
-
+    data.selected_timeslot.starttime = new Date(val.start.value);
+    data.selected_timeslot.endtime = new Date(val.end.value);
+    data.num_slots = 2;
+    data.rental.slots = [];
+    data.rental.slots.push( { customer_id: userview.id, customer_string: userview.custy_string } );
+    data.rental.slots.push( { customer_id: 0, customer_string: "Add Student" } );
+    calculate_total();
   });
 
 });
