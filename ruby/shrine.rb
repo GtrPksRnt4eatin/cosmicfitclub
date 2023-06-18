e08a96c5c5cc26fb3b826c7f49af7ef9d6a0275e
Maxwell Klein <ben@cosmicfitclub.com>
2023-06-18 13:23:16 -0400

    payroll improvements, shrine upgrade

--- ruby/shrine.rb
+++ ruby/shrine.rb
@@ -14,20 +14,26 @@ Shrine.plugin :rack_file
 
 class ImageUploader < Shrine 
   
-  include ImageProcessing::MiniMagick
+  #include ImageProcessing::MiniMagick
   
-  plugin :processing
-  plugin :versions
+  #plugin :processing
+  #plugin :versions
+  plugin :derivatives, create_on_promote: true, versions_compatibility: true
 
-  process(:store) do |io, context|
+  #process(:store) do |io, context|
+  #  original = io.download
+  #  size_800 = resize_to_limit(original, 800, 800)
+  #  size_500 = resize_to_limit(size_800, 500, 500)
+  #  size_300 = resize_to_limit(size_500, 300, 300)
+  #  { original: original, large: size_800, medium: size_500, small: size_300 }
+  #end
 
-    original = io.download
-    size_800 = resize_to_limit(original, 800, 800)
-    size_500 = resize_to_limit(size_800, 500, 500)
-    size_300 = resize_to_limit(size_500, 300, 300)
-
-    { original: original, large: size_800, medium: size_500, small: size_300 }
-
+  Attacher.derivatives do |original|
+    magick = ImageProcessing::MiniMagick.source(original)
+    { large:  magick.resize_to_limit!(800,800),
+      medium: magick.resize_to_limit!(500,500),
+      small:  magick.resize_to_limit!(300,300)
+    }
   end
 
 end
