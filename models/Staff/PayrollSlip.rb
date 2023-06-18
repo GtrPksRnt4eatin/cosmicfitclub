3f7425c05f2245072c8df8dd7b128eb0d89a54ca
Maxwell Klein <ben@cosmicfitclub.com>
2023-06-18 03:07:11 -0400

    payroll improvements

--- models/Staff/PayrollSlip.rb
+++ models/Staff/PayrollSlip.rb
@@ -11,8 +11,8 @@ class PayrollSlip < Sequel::Model(:payroll_slips)
   end
 
   def totals
-    { :payout_total => self.lines.inject { |sum,x| sum + x.value }
-      :cosmic_total => self.lines.inject { |sum,x| sum + x.cosmic }
+    { :payout_total => self.lines.inject { |sum,x| sum + x.value },
+      :cosmic_total => self.lines.inject { |sum,x| sum + x.cosmic },
       :loft_total   => self.lines.inject { |sum,x| sum + x.loft }
     }
   end
