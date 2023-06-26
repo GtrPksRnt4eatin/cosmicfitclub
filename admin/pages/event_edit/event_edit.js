b2f5a1fa9a88df8e2a2ee53f78293b2992b7912e
Benjamin Maxwell <bklein261@gmail.com>
2023-06-26 17:24:03 -0400

    Update event_edit.js

--- admin/pages/event_edit/event_edit.js
+++ admin/pages/event_edit/event_edit.js
@@ -78,7 +78,7 @@ ctrl = {
   add_collaborator: function(e,m) {
     custy_selector.show_modal(null, function(custy_id) {
       let payload = { event_id: data.event.id, customer_id: custy_id }
-      $.post('/models/events/collab', payload)
+      $.post('/models/events/collabs', payload)
        .done( function() { fetch_event(); } )
     });
   },
