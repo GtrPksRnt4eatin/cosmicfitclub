01cb96c824a3bb00b094bedb51f0541a6b561770
Benjamin Maxwell <bklein261@gmail.com>
2023-06-26 18:52:11 -0400

    Update EventCollaboration.rb

--- models/Events/EventCollaboration.rb
+++ models/Events/EventCollaboration.rb
@@ -4,4 +4,15 @@ class EventCollaboration < Sequel::Model
   many_to_one :customer
   many_to_one :event
 
+  def details_view
+    { customer: self.customer.to_list_hash,
+      label: self.customer.to_list_string,
+      event_id: self.event_id,
+      phone: self.customer.phone,
+      stripe_connect_id: self.customer.staff[0] ? self.customer.staff[0].stripe_connect_id : '',
+      percentage: self.percentage,
+      notify: self.notify     
+    }
+  end
+
 end
