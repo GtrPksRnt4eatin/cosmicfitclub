39d932ad63fc16bd6bf2ded99d245adaea438f81
Benjamin Maxwell <bklein261@gmail.com>
2023-07-15 16:01:47 -0400

    Update group_timeslots to show current week

--- admin/pages/group_timeslots/group_timeslots.js
+++ admin/pages/group_timeslots/group_timeslots.js
@@ -15,12 +15,14 @@ $(document).ready( function() {
 });
 
   function setup_daypilot() {
+    let start = (new Date).toISOString().split('T')[0];
+    let end = new Date(Date.now() + 7*24*60*60*1000).toISOString().split('T')[0];
     daypilot = new DayPilot.Calendar('daypilot', {
       viewType: "Days",
       days: 7,
       cellDuration: 30,
       cellHeight: 40,
-      startDate:  (new Date).toISOString().split('T')[0],
+      startDate:  start,
       headerDateFormat: "ddd MMM d",
       businessBeginsHour: 10,
       businessEndsHour: 23,
@@ -37,7 +39,7 @@ $(document).ready( function() {
       }
     });
   
-    $.get("/models/groups/range-admin/2021-08-09/2021-08-16")
+    $.get(`/models/groups/range-admin/${start}/${end}`)
     .success( function(val) {
       for(i=0; i<val.length; i++) {
         daypilot.events.add(val[i]);
