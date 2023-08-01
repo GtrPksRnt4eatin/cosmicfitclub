a5c28caad155266d9b64634713944bf99ad2d75c
Ben Klein <bklein261@gmail.com>
2023-08-01 17:06:46 -0400

    loft calendar

--- admin/pages/group_timeslots/group_timeslots.js
+++ admin/pages/group_timeslots/group_timeslots.js
@@ -2,7 +2,6 @@
 var loft_calendar;
 
 $(document).ready( function() {
-
   popupmenu      = new PopupMenu( id('popupmenu_container') );
   custy_selector = new CustySelector();
 
@@ -11,42 +10,10 @@ $(document).ready( function() {
   custy_selector.show_add_form();
   
   var view = rivets.bind($('body'), { data : {} } );
-
   loft_calendar = get_element(view,'loft-calendar');
-  
 });
 
 window.addEventListener('pageshow', fetch_data);
-
-//function setup_daypilot() {
-//  start = (new Date).toISOString().split('T')[0];
-//  end = new Date(Date.now() + 7*24*60*60*1000).toISOString().split('T')[0];
-//  daypilot = new DayPilot.Calendar('daypilot', {
-//    viewType: "Days",
-//    days: 7,
-//    cellDuration: 30,
-//    cellHeight: 30,
-//    startDate:  start,
-//    headerDateFormat: "ddd MMM d",
-//    businessBeginsHour: 10,
-//    businessEndsHour: 23,
-//    dayBeginsHour: 10,
-//    dayEndsHour: 23,
-//    timeRangeSelectedHandling: "Disabled",
-//    eventDeleteHandling: "Disabled",
-//    eventMoveHandling: "Disabled",
-//    eventResizeHandling: "Disabled",
-//    eventHoverHandling: "Disabled",
-//    onBeforeEventRender:   function(args) {
-//      args.data.html = args.data.text.split(',').join(',<br/>');
-//    },
-//    onEventClick: function(args) {
-//      window.location = '/checkout/group/' + args.e.data.id;
-//    }
-//  });
-//  
-//  daypilot.init();
-//}
 
 function fetch_data() {
   loft_calendar && loft_calendar.refresh_data();
