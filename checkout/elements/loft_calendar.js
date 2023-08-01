ed92c93e27ff522846964039bae6f3855f9f689e
Ben Klein <bklein261@gmail.com>
2023-08-01 16:53:49 -0400

    loft calendar

--- checkout/elements/loft_calendar.js
+++ checkout/elements/loft_calendar.js
@@ -95,11 +95,11 @@ LoftCalendar.prototype = {
     this.state.daypilot.events.list = [];
     //this.state.daypilot.update();
     this.get_reservations()
-      .then(this.get_gcal_events()
-        .then(this.state.daypilot.update()
+      .then(function() { this.get_gcal_events()
+        .then(function() { this.state.daypilot.update()
           .then(function() { this.state.loading = false; }.bind(this))
-        )
-      );
+        }.bind(this))
+      }.bind(this));
   }
 }
 
