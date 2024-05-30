7b3ae3bd67b50070823cb684afb560523816c5b5
Benjamin Maxwell <bklein261@gmail.com>
2024-05-30 17:27:51 -0400

    Update package_selector.js

--- frontdesk/elements/package_selector.js
+++ frontdesk/elements/package_selector.js
@@ -29,7 +29,7 @@ PackageSelector.prototype.HTML = `
   <div>
     <div class='title'>Choose A Package</div>
     <div>
-      <select rv-on-change='this.select'>
+      <select rv-on-change='this.select' rv-idselect='state.package_id' >
         <option value='0' data-price='0'> None </option>
         <option rv-each-pack="state.packages" rv-value='pack.id'>
           { pack.formatted_price } { pack.name }
