4edf086779da4f76b7814657ca104aa44989ec43
Benjamin Maxwell <bklein261@gmail.com>
2024-07-04 19:20:48 -0400

    Update door.js

--- admin/pages/door/door.js
+++ admin/pages/door/door.js
@@ -1,22 +1,18 @@
 $(document).ready(function() {
+  setInterval(function() {
+    $.get('http://192.168.1.16/cm?cmnd=Power', function(resp) {
+       console.log(resp);
+    });
+  }, 500);
+ 
   $('#up').on('click', function(e) {
-    $.post('/door/up');
+    $.post('http://192.168.1.16/cm?cmnd=Power%20On');
     cancelEvent(e);
   });
 
-  $('#down').on('mousedown touchstart', function(e) {
-  	cancelEvent(e);
-    $.post('/door/down', 'val=1' );
-  });
-
-  $('#down').on('mouseup touchend mouseout', function(e) {
-  	cancelEvent(e);
-  	$.post('/door/down', 'val=0' );
-  });
-
-  $('#stop').on('mousedown touchend', function(e) {
-  	cancelEvent(e);
-    $.post('/door/stop');
-  });
+  $('#down').on('click', function(e) {
+    $.post('http://192.168.1.16/cm?cmnd=Power%20Off');
+    cancelEvent(e);
+  });  
 
 });
