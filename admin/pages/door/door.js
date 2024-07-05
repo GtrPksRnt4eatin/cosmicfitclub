33688b5248ff9b8d5a860f2eb073d6e7ee593eec
Benjamin Maxwell <bklein261@gmail.com>
2024-07-04 20:22:07 -0400

    Update door.js

--- admin/pages/door/door.js
+++ admin/pages/door/door.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
   setInterval(function() {
     $.get('/door/status', function(resp) {
+       switch(resp?.POWER) {
+         case 'ON': 
+           $('#up').css('background', 'rgba(0,255,0,0.2)');
+           $('#down').css('background', 'rgba(255,255,255,0.2)');
+           break;
+         case 'OFF':
+           $('#down').css('background', 'rgba(255,0,0,0.2)');
+           $('#up').css('background', 'rgba(255,255,255,0.2)');
+           break;          
+       }
        console.log(resp);
        $('#status').innerHTML = resp;
     }, 'json');
