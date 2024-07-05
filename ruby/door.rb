e7e191df69b9fe0afb5fba637223e913e7908dd2
Benjamin Maxwell <bklein261@gmail.com>
2024-07-05 00:53:44 -0400

    Update door.rb

--- ruby/door.rb
+++ ruby/door.rb
@@ -5,33 +5,24 @@ require_relative '../auth/auth.rb'
 class Door < Sinatra::Base
   
   register Sinatra::Auth
-
-  post('/up', :auth=> 'door') do
-    RestClient.patch( 'http://cosmicfitclub.ddns.net:5000/api/v1/pin/26', '{ "value": 1 }', :content_type => 'application/json', :timeout => 3 )
-    sleep(0.5)
-    RestClient.patch( 'http://cosmicfitclub.ddns.net:5000/api/v1/pin/26',  '{ "value": 0 }', :content_type => 'application/json', :timeout => 3 )
-    Slack.website_access "#{Time.now.strftime("%m/%d/%Y %I:%M:%S %p")} Door Opened By #{customer.nil? ? "Unknown" : customer.name}"
-  end
-
-  post('/down', :auth=> 'door') do
-    payload = { :value => params[:val].to_i }.to_json
-    RestClient.patch( 'http://cosmicfitclub.ddns.net:5000/api/v1/pin/6', payload , :content_type => 'application/json', :timeout => 3 )
-  end
-
-  post('/stop', :auth=> 'door') do
-    RestClient.patch( 'http://cosmicfitclub.ddns.net:5000/api/v1/pin/22', { :value => 1 }.to_json, :content_type => 'application/json', :timeout => 3 ) 
-    sleep(0.5)
-    RestClient.patch( 'http://cosmicfitclub.ddns.net:5000/api/v1/pin/22', { :value => 0 }.to_json, :content_type => 'application/json', :timeout => 3 ) 
+  
+  post('/momentary', :auth=> 'door') do
+    RestClient.get( 'http://72.231.24.250:86/cm?cmnd=Power%20On', :content_type => 'application/json', :timeout => 3 )
+    Slack.website_access "#{Time.now.strftime("%m/%d/%Y %I:%M:%S %p")} Door Momentarily Opened By #{customer.nil? ? "Unknown" : customer.name}"
+    sleep(10)
+    RestClient.get( 'http://72.231.24.250:86/cm?cmnd=Power%20Off', :content_type => 'application/json', :timeout => 3 )
   end
 
   post('/open', :auth=> 'door') do
     RestClient.get( 'http://72.231.24.250:86/cm?cmnd=Power%20On', :content_type => 'application/json', :timeout => 3 )
+    Slack.website_access "#{Time.now.strftime("%m/%d/%Y %I:%M:%S %p")} Door Opened By #{customer.nil? ? "Unknown" : customer.name}"
   end
 
   post('/close', :auth=> 'door') do
     RestClient.get( 'http://72.231.24.250:86/cm?cmnd=Power%20Off', :content_type => 'application/json', :timeout => 3 )
+    Slack.website_access "#{Time.now.strftime("%m/%d/%Y %I:%M:%S %p")} Door Closed By #{customer.nil? ? "Unknown" : customer.name}"
   end
-
+  
   get('/status', :auth=> 'door') do
     RestClient.get( 'http://72.231.24.250:86/cm?cmnd=Power', :content_type=>'application/json', :timeout=>3)
   end
