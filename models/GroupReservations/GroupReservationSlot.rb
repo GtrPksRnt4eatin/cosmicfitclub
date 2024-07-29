c2916ac0f1e79b4b2819e55aef2431d6645c1137
Benjamin Maxwell <bklein261@gmail.com>
2024-07-29 18:32:18 -0400

    Update GroupReservationSlot.rb

--- models/GroupReservations/GroupReservationSlot.rb
+++ models/GroupReservations/GroupReservationSlot.rb
@@ -5,6 +5,10 @@ class GroupReservationSlot < Sequel::Model
     many_to_one :payment,     :class => :CustomerPayment,         :key => :payment_id
     one_to_one  :checkin,     :class => :GroupReservationCheckin, :key => :slot_id
 
+    def after_save
+      self.reservation.publish_gcal_event
+    end
+
     def details_view
       hsh = self.to_hash
       hsh[:customer] = self.customer.try(:to_token)
