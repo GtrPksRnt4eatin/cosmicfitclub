af3e76805440b7c089c5501346d2b6dd9e5ba96f
Benjamin Maxwell <bklein261@gmail.com>
2024-07-29 20:07:40 -0400

    Update GroupReservationSlot.rb

--- models/GroupReservations/GroupReservationSlot.rb
+++ models/GroupReservations/GroupReservationSlot.rb
@@ -1,7 +1,7 @@
 class GroupReservationSlot < Sequel::Model
 
     many_to_one :customer
-    many_to_one :reservation, :class => :GroupReservation,        :key => :reservation_id
+    many_to_one :reservation, :class => :GroupReservation,        :key => :group_reservation_id
     many_to_one :payment,     :class => :CustomerPayment,         :key => :payment_id
     one_to_one  :checkin,     :class => :GroupReservationCheckin, :key => :slot_id
 
