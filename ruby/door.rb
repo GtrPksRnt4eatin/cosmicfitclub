0b88095b51a515731e7550b3a2686b0ecd228a92
Benjamin Maxwell <bklein261@gmail.com>
2024-09-13 01:31:37 -0400

    Update door.rb

--- ruby/door.rb
+++ ruby/door.rb
@@ -7,24 +7,24 @@ class Door < Sinatra::Base
   register Sinatra::Auth
   
   post('/momentary', :auth=> 'door') do
-    RestClient.get( 'http://72.231.24.250:86/cm?cmnd=Power%20On', :content_type => 'application/json', :timeout => 3 )
+    RestClient.get( 'http://66.108.37.62:86/cm?cmnd=Power%20On', :content_type => 'application/json', :timeout => 3 )
     Slack.website_access "#{Time.now.strftime("%m/%d/%Y %I:%M:%S %p")} Door Momentarily Opened By #{customer.nil? ? "Unknown" : customer.name}"
     sleep(10)
-    RestClient.get( 'http://72.231.24.250:86/cm?cmnd=Power%20Off', :content_type => 'application/json', :timeout => 3 )
+    RestClient.get( 'http://66.108.37.62:86/cm?cmnd=Power%20Off', :content_type => 'application/json', :timeout => 3 )
   end
 
   post('/open', :auth=> 'door') do
-    RestClient.get( 'http://72.231.24.250:86/cm?cmnd=Power%20On', :content_type => 'application/json', :timeout => 3 )
+    RestClient.get( 'http://66.108.37.62:86/cm?cmnd=Power%20On', :content_type => 'application/json', :timeout => 3 )
     Slack.website_access "#{Time.now.strftime("%m/%d/%Y %I:%M:%S %p")} Door Opened By #{customer.nil? ? "Unknown" : customer.name}"
   end
 
   post('/close', :auth=> 'door') do
-    RestClient.get( 'http://72.231.24.250:86/cm?cmnd=Power%20Off', :content_type => 'application/json', :timeout => 3 )
+    RestClient.get( 'http://66.108.37.62:86/cm?cmnd=Power%20Off', :content_type => 'application/json', :timeout => 3 )
     Slack.website_access "#{Time.now.strftime("%m/%d/%Y %I:%M:%S %p")} Door Closed By #{customer.nil? ? "Unknown" : customer.name}"
   end
   
   get('/status', :auth=> 'door') do
-    RestClient.get( 'http://72.231.24.250:86/cm?cmnd=Power', :content_type=>'application/json', :timeout=>1)
+    RestClient.get( 'http://66.108.37.62:86/cm?cmnd=Power', :content_type=>'application/json', :timeout=>1)
   end
 
 end
