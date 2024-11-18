0f5ff00d64ba2d3231aa8122113fa3de2b8448d9
Maxwell Klein <ben@cosmicfitclub.com>
2024-11-17 22:12:47 -0500

    tagging in

--- models/Access/NfcTagRoutes.rb
+++ models/Access/NfcTagRoutes.rb
@@ -4,12 +4,12 @@ class NfcTagRoutes < Sinatra::Base
 
   post '/' do
   	tag = NfcTag[ :value => params[:value] ]
-    if(tag && tag.customer) {
+    if (tag && tag.customer)
       RestClient.get( 'http://66.108.37.62:86/cm?cmnd=Power%20On', :content_type => 'application/json', :timeout => 3 )
       Slack.website_access "#{Time.now.strftime("%m/%d/%Y %I:%M:%S %p")} Door Tagged Open By #{tag.customer.name}"
       sleep(10)
       RestClient.get( 'http://66.108.37.62:86/cm?cmnd=Power%20Off', :content_type => 'application/json', :timeout => 3 )
-    }
+    end
     tag.customer.to_json
   end
 
