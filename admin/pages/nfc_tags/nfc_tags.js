af43e5874c27686160104bf4b0fc860f620bf97f
Maxwell Klein <ben@cosmicfitclub.com>
2024-12-04 04:16:41 -0500

    nfc admin

--- admin/pages/nfc_tags/nfc_tags.js
+++ admin/pages/nfc_tags/nfc_tags.js
@@ -3,6 +3,13 @@ data = {
 }
 
 ctrl = {
+  load_custy(custy) { data.customer = custy; },
+  add_tag() { 
+    let x = 5;
+  },
+  del_tag(e,m) { 
+    let x=5;
+  }
 }
 
 $(document).ready(function() {
