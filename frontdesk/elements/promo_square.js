537ec6b3ea4d6f08395110a322ed7e46de3cc378
Maxwell Klein <ben@cosmicfitclub.com>
2024-12-11 15:25:23 -0500

    promosquare

--- frontdesk/elements/promo_square.js
+++ frontdesk/elements/promo_square.js
@@ -46,7 +46,7 @@ Object.assign( PromoSquare.prototype, element);
 
 PromoSquare.prototype.HTML = `
   <img rv-src="state.current.image_url"/>
-  <div rv-each-line="state.currrent.poster_lines">{line}</div>
+  <div rv-each-line="state.current.poster_lines">{line}</div>
 `.untab(2);
 
 PromoSquare.prototype.CSS = `
