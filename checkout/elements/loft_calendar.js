5f04d64d7176341c1fa98a6d5459b52df07e57fd
Maxwell Klein <ben@cosmicfitclub.com>
2024-12-23 22:28:10 -0500

    add locations to calendar

--- checkout/elements/loft_calendar.js
+++ checkout/elements/loft_calendar.js
@@ -84,6 +84,7 @@ LoftCalendar.prototype = {
         this.state.reservations.for_each( function(res) {
           let gcal = this.state.daypilot.events.find(res.gcal);
           if(gcal) {
+            res.data ||= {};
             res.data.resource = gcal.resource();
             this.state.daypilot.events.remove(gcal);
           }
