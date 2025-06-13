90633081a88d0d02f62f9338981fec7a2056531f
Maxwell Klein <ben@cosmicfitclub.com>
2025-06-13 19:23:37 -0400

    payroll_slips

--- models/Staff/Payout.rb
+++ models/Staff/Payout.rb
@@ -4,4 +4,27 @@ class Payout < Sequel::Model(:payouts)
   many_to_one :payroll_slip
   many_to_one :staff
 
+  def stripe_account_id
+    return nil unless self.staff && self.staff.stripe_connected_acct
+    self.staff.stripe_connected_acct  
+  end
+
+  def stripe_destination
+    return nil unless self.stripe_payout_id
+    payout = Stripe::Payout.retrieve(self.stripe_payout_id, { stripe_account: stripe_account_id })
+    return payout.destination if payout && payout.destination
+    nil
+  rescue Stripe::InvalidRequestError
+    nil
+  end
+
+  def stripe_arrival_date
+    return nil unless self.stripe_payout_id
+    payout = Stripe::Payout.retrieve(self.stripe_payout_id, { stripe_account: stripe_account_id })
+    return Time.at(payout['arrival_date']) if payout && payout['arrival_date']
+    nil
+  rescue Stripe::InvalidRequestError
+    nil
+  end
+
 end
