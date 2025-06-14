999d45ec675f8584cd9821528fd265b50e985c5f
Maxwell Klein <ben@cosmicfitclub.com>
2025-06-13 20:39:57 -0400

    sequel through

--- models/Staff/Payout.rb
+++ models/Staff/Payout.rb
@@ -4,14 +4,14 @@ class Payout < Sequel::Model(:payouts)
   many_to_one :payroll_slip
   many_to_one :staff
 
-  def stripe_account_id
-    return nil unless self.staff && self.staff.stripe_account_id
-    self.staff.stripe_account_id  
+  def stripe_connect_id
+    return nil unless self.staff && self.staff.stripe_connect_id
+    self.staff.stripe_connect_id  
   end
 
   def stripe_destination
     return nil unless self.stripe_payout_id
-    payout = Stripe::Payout.retrieve(self.stripe_payout_id, { stripe_account: stripe_account_id })
+    payout = Stripe::Payout.retrieve(self.stripe_payout_id, { stripe_account: stripe_connect_id })
     return payout.destination if payout && payout.destination
     nil
   rescue Stripe::InvalidRequestError
@@ -20,7 +20,7 @@ class Payout < Sequel::Model(:payouts)
 
   def stripe_arrival_date
     return nil unless self.stripe_payout_id
-    payout = Stripe::Payout.retrieve(self.stripe_payout_id, { stripe_account: stripe_account_id })
+    payout = Stripe::Payout.retrieve(self.stripe_payout_id, { stripe_account: stripe_connect_id })
     return Time.at(payout['arrival_date']) if payout && payout['arrival_date']
     nil
   rescue Stripe::InvalidRequestError
