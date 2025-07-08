e0c130358c872324f3ef3970ef64c51137d2f414
Maxwell Klein <ben@cosmicfitclub.com>
2025-07-07 20:37:15 -0400

    email door link

--- ruby/door.rb
+++ ruby/door.rb
@@ -5,7 +5,17 @@ require_relative '../auth/auth.rb'
 class Door < Sinatra::Base
   
   register Sinatra::Auth
-  
+ 
+  get('/email_link') do
+    reservation = GroupReservation[ :tag => params[:tag] ] or return "Invalid Reservation Tag"
+    if((reservation.start_time - 30.minutes) > Time.now) then return "This link will not work until #{(reservation.start_time - 30.minutes ).strftime("%m/%d/%Y %I:%M:%S %p")}" end
+    if((reservation.end_time + 30.minutes)   < Time.now) then return "This link has expired as of #{(reservation.end_time + 30.minutes).strftime("%m/%d/%Y %I:%M:%S %p")}" end
+    RestClient.get( 'http://loft.cosmicfitclub.com:86/cm?cmnd=Power%20On', :content_type => 'application/json', :timeout => 3 )
+    sleep(15)
+    RestClient.get( 'http://loft.cosmicfitclub.com:86/cm?cmnd=Power%20Off', :content_type => 'application/json', :timeout => 3 )
+    return "The door has been opened and will remain open for 15 seconds."
+  end
+    
   post('/momentary', :auth=> 'door') do
     RestClient.get( 'http://66.108.37.62:86/cm?cmnd=Power%20On', :content_type => 'application/json', :timeout => 3 )
     Slack.website_access "#{Time.now.strftime("%m/%d/%Y %I:%M:%S %p")} Door Momentarily Opened By #{customer.nil? ? "Unknown" : customer.name}"
