fa41e47dc90d1e9846741504b49368ef45167e81
Maxwell Klein <ben@cosmicfitclub.com>
2025-07-28 12:23:19 -0400

    fix fonts for loft checkout

--- ruby/door.rb
+++ ruby/door.rb
@@ -3,10 +3,6 @@ require 'sucker_punch'
 
 require_relative '../auth/auth.rb'
 
-
-
-
-
 class Door < Sinatra::Base
   
   register Sinatra::Auth
@@ -27,38 +23,30 @@ class Door < Sinatra::Base
   end
 
   post('/open', :auth=> 'door') do
-    RestClient.get( 'http://66.108.37.62:86/cm?cmnd=Power%20On', :content_type => 'application/json', :timeout => 3 )
+    RestClient.get( 'http://loft.cosmicfitclub.com:86/cm?cmnd=Power%20On', :content_type => 'application/json', :timeout => 3 )
     Slack.website_access "#{Time.now.strftime("%m/%d/%Y %I:%M:%S %p")} Door Opened By #{customer.nil? ? "Unknown" : customer.name}"
   end
 
   post('/close', :auth=> 'door') do
-    RestClient.get( 'http://66.108.37.62:86/cm?cmnd=Power%20Off', :content_type => 'application/json', :timeout => 3 )
+    RestClient.get( 'http://loft.cosmicfitclub.com:86/cm?cmnd=Power%20Off', :content_type => 'application/json', :timeout => 3 )
     Slack.website_access "#{Time.now.strftime("%m/%d/%Y %I:%M:%S %p")} Door Closed By #{customer.nil? ? "Unknown" : customer.name}"
   end
   
   get('/status', :auth=> 'door') do
-    RestClient.get( 'http://66.108.37.62:86/cm?cmnd=Power', :content_type=>'application/json', :timeout=>1)
+    RestClient.get( 'http://loft.cosmicfitclub.com:86/cm?cmnd=Power', :content_type=>'application/json', :timeout=>1)
   end
 
   post('/tag') do
     tag = NfcTag[ :value => params[:value] ] 
     tag or ( Slack.website_access "Tag Scanned: #{params[:value]}"; halt 401 )
-    status = RestClient.get( 'http://66.108.37.62:86/cm?cmnd=Power', :content_type=>'application/json', :timeout=>1)
+    status = RestClient.get( 'http://loft.cosmicfitclub.com:86/cm?cmnd=Power', :content_type=>'application/json', :timeout=>1)
     status = JSON.parse(status)
     DoorUnlockMomentary.perform_async(15, status["POWER"].capitalize)
     Slack.website_access "#{Time.now.strftime("%m/%d/%Y %I:%M:%S %p")} Door Tagged By #{tag.customer.name}"
     status 204
-    #RestClient.get( 'http://66.108.37.62:86/cm?cmnd=Power%20On', :content_type => 'application/json', :timeout => 3 )
-    #sleep(8)
-    #RestClient.get( "http://66.108.37.62:86/cm?cmnd=Power%20#{status["POWER"].capitalize}", :content_type => 'application/json', :timeout => 3 )
   end
 
 end
-
-# DoorUnlockMomentary is a SuckerPunch job that unlocks the door for a specified number of seconds
-# and then returns it to a specified state (default is "Off").
-# It uses the RestClient to send HTTP GET requests to the door's API.
-# The job can be performed asynchronously, allowing for non-blocking execution.
 
 class DoorUnlockMomentary
   include SuckerPunch::Job
