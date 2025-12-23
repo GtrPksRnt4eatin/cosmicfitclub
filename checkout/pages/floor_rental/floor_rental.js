0a30501b671c2fe776d1edc7c2bc2ee3e83b0c4a
Maxwell Klein <ben@cosmicfitclub.com>
2025-12-23 16:55:53 -0500

    money formatting

--- checkout/pages/floor_rental/floor_rental.js
+++ checkout/pages/floor_rental/floor_rental.js
@@ -24,10 +24,32 @@ data = {
   
     userview          = new UserView(id('userview_container'));
     popupmenu         = new PopupMenu( id('popupmenu_container') );
+    payment_form      = new PaymentForm();
     loft_calendar     = get_element(view,'loft-calendar');
     reservations_list = get_element(view,'reservations-list');
     group_timeslot    = get_element(view,'group-timeslot');
     custy_selector    = new CustySelector();
+
+    payment_form.customer_facing();
+    payment_form.ev_sub('show', popupmenu.show);
+    payment_form.ev_sub('hide', popupmenu.hide);
+    
+    $('.buy-button').on('click', function(e) {
+      e.preventDefault();
+      if (!userview.logged_in) { userview.onboard(); return; }
+      
+      const button = $(this);
+      const type = button.data('type');
+      const id = button.data('id');
+      const name = button.data('name');
+      const amount = button.data('amount');
+      
+      payment_form.checkout(userview.id, amount, name, { type: type, id: id }, 
+        function(payment_id) { 
+          completePurchase(type, id, payment_id);
+        }
+      );
+    });
 
     userview.ev_sub('on_user', function() {
       $.get(`/models/customers/${userview.id}/class_passes`, function(resp) { data.class_passes = isNaN(resp) ? 0 : resp; }, 'json');
@@ -51,4 +73,23 @@ data = {
     });
     
   });
+
+  function completePurchase(type, id, payment_id) {
+    let endpoint = '/checkout/pack/buy';
+    let data = { 
+      payment_id: payment_id,
+      customer_id: userview.id,
+      pack_id: id
+    };
+    
+    $.post(endpoint, data, 'json')
+     .done(function() { 
+       alert('Purchase successful! Your passes have been added.');
+       window.location.reload(); 
+     })
+     .fail(function(e) { 
+       alert('There was an error completing your purchase. Please contact support.'); 
+       console.error(e); 
+     });
+  }
   
