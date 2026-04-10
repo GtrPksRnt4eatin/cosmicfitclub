0e49c98252449345410714d4fc2cc4d5cdbc3e8f
Maxwell Klein <ben@cosmicfitclub.com>
2026-04-10 14:16:13 -0400

    facebook promos

--- integrations/facebook.rb
+++ integrations/facebook.rb
@@ -1,21 +1,54 @@
-require 'koala'
+require 'rest-client'
+require 'json'
 
 module Facebook
-  
-  Facebook::API = Koala::Facebook::API.new(ENV['FACEBOOK_PAGE_TOKEN'])
+  API_VERSION = "v21.0"
+  BASE_URL    = "https://graph.facebook.com/#{API_VERSION}"
+  PAGE_TOKEN  = ENV['FB_PROMO_TOKEN']
 
-  def Facebook.event_list
-  	Facebook::API.get_connections("me", "events")
+  def self.get(path, params = {})
+    response = RestClient.get("#{BASE_URL}/#{path}", params: params.merge(access_token: PAGE_TOKEN))
+    JSON.parse(response.body)
+  rescue RestClient::ExceptionWithResponse => e
+    JSON.parse(e.response.body)
   end
 
+  def self.post(path, params = {})
+    response = RestClient.post("#{BASE_URL}/#{path}", params.merge(access_token: PAGE_TOKEN))
+    JSON.parse(response.body)
+  rescue RestClient::ExceptionWithResponse => e
+    JSON.parse(e.response.body)
+  end
+
+  def self.event_list
+    get("me/events")["data"] || []
+  end
+
+  # Post a text or link promotion to the page feed
+  def self.post_promotion(message, link: nil)
+    params = { message: message }
+    params[:link] = link if link
+    post("me/feed", params)
+  end
+
+  # Post a photo promotion to the page feed
+  def self.post_photo(image_url, caption: nil)
+    params = { url: image_url, published: true }
+    params[:caption] = caption if caption
+    post("me/photos", params)
+  end
+
+  # Post a photo story to the page
+  def self.post_story(image_url)
+    post("me/photo_stories", { url: image_url })
+  end
 end
 
 class FacebookRoutes < Sinatra::Base
 
   get '/event_list' do
-  	content_type :json
-  	list = Facebook.event_list.map(&:to_json)
-    "[#{list.join(',')}]"
+    content_type :json
+    Facebook.event_list.to_json
   end
 
 end
